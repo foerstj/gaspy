02f3da1fe16e8c4e0951f64ab772637fccfbc491
Johannes Förstner <johannes@famfoerstner.de>
2021-02-16 22:24:49 +0100

    Print some more correlating XP stats

--- csv.py
+++ csv.py
@@ -111,6 +111,7 @@ def enemy_occurrence(bits: Bits):
     maps = {n: bits.maps[n] for n in maps}
     enemies = load_enemies(bits)
     enemy_regions = {e.template_name: list() for e in enemies}
+    enemies_by_tn = {e.template_name: e for e in enemies}
     for map_name, map in maps.items():
         print('Map ' + map_name)
         region_xp = load_region_xp(map)
@@ -118,15 +119,15 @@ def enemy_occurrence(bits: Bits):
             region = rxp.region
             region_enemies = region.get_enemies()
             region_enemy_template_names = {e.template_name for e in region_enemies}
-            region_enemy_strs = region_enemy_template_names
+            region_enemy_strs = [tn + ' ('+str(enemies_by_tn[tn].xp)+' XP)' for tn in region_enemy_template_names]
             region_enemies_str = str(len(region_enemy_template_names)) + ' enemy types: ' + ', '.join(region_enemy_strs)
             print('Region ' + region.get_name() + ' (XP '+str(rxp.xp_pre)+' - '+str(rxp.xp_post)+') contains ' + region_enemies_str)
             for retn in region_enemy_template_names:
                 enemy_regions[retn].append(rxp)
     for enemy in enemies:
-        rxp = enemy_regions[enemy.template_name]
-        region_strs = [r.name for r in rxp]
-        regions_str = str(len(rxp)) + ' regions: ' + ', '.join(region_strs)
+        rxps = enemy_regions[enemy.template_name]
+        region_strs = [rxp.name + ' (XP '+str(rxp.xp_pre)+' - '+str(rxp.xp_post)+')' for rxp in rxps]
+        regions_str = str(len(rxps)) + ' regions: ' + ', '.join(region_strs)
         print('Enemy type ' + enemy.template_name + ' (' + str(enemy.xp) + ' XP) occurs in ' + regions_str)
 
 
