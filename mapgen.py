90a9187b331201d78f1ef88c2d664e6de913c4fd
Johannes Förstner <johannes@famfoerstner.de>
2021-02-27 20:21:57 +0100

    Rectangular maps

--- mapgen.py
+++ mapgen.py
@@ -41,39 +41,44 @@ class Plant:
 
 
 class FlatTerrain2D:
-    def __init__(self, size=0):
-        assert size % 4 == 0
-        self.size = size
-        node_size = int(size / 4)
-        self.node_size = node_size
-        nodes_2d = [[TerrainNode(None, 't_xxx_flr_04x04-v0', 'grs01') for _ in range(node_size)] for _ in range(node_size)]
+    def __init__(self, size_x=0, size_z=0):
+        assert size_x % 4 == 0
+        assert size_z % 4 == 0
+        self.size_x = size_x
+        self.size_z = size_z
+        node_size_x = int(size_x / 4)
+        node_size_z = int(size_z / 4)
+        self.node_size_x = node_size_x
+        self.node_size_z = node_size_z
+        nodes_2d = [[TerrainNode(None, 't_xxx_flr_04x04-v0', 'grs01') for _ in range(node_size_z)] for _ in range(node_size_x)]
         self.nodes_2d = nodes_2d
-        for x in range(node_size):
-            for z in range(node_size):
+        for x in range(node_size_x):
+            for z in range(node_size_z):
                 node = nodes_2d[x][z]
                 if x > 0:
                     nn = nodes_2d[x-1][z]
                     node.connect_doors(2, nn, 4)
-                if x < node_size-1:
+                if x < node_size_x-1:
                     nn = nodes_2d[x+1][z]
                     node.connect_doors(4, nn, 2)
                 if z > 0:
                     nn = nodes_2d[x][z-1]
                     node.connect_doors(1, nn, 3)
-                if z < node_size-1:
+                if z < node_size_z-1:
                     nn = nodes_2d[x][z+1]
                     node.connect_doors(3, nn, 1)
         self.plants: list[Plant] = []
 
     def make_terrain(self):
         terrain = Terrain()
-        for x in range(self.node_size):
-            for z in range(self.node_size):
+        for x in range(self.node_size_x):
+            for z in range(self.node_size_z):
                 node = self.nodes_2d[x][z]
                 node.guid = terrain.new_node_guid()
                 terrain.nodes.append(node)
-        i2d_target_node = int(self.node_size / 2)
-        target_node = self.nodes_2d[i2d_target_node][i2d_target_node]
+        i_tn_x = int(self.node_size_x / 2)
+        i_tn_z = int(self.node_size_z / 2)
+        target_node = self.nodes_2d[i_tn_x][i_tn_z]
         terrain.target_node = target_node
         return terrain
 
@@ -97,11 +102,13 @@ class FlatTerrain2D:
 
 def create_plants(flat_terrain_2d: FlatTerrain2D):
     template_names = ['aloe_des_01', 'bush_grs_04', 'cornstalk_glb_grn_01', 'flowers_grs_04', 'flowers_grs_05', 'groundcover_grs_03', 'mushroom_glb_10', 'mushrooms_cav_06']
-    num_circles = max(3, random.randint(flat_terrain_2d.node_size, flat_terrain_2d.node_size*4))
-    map_center = flat_terrain_2d.size / 2
+    size = min(flat_terrain_2d.size_x, flat_terrain_2d.size_z)
+    num_circles = max(3, random.randint(int(size/4), size))
+    map_center_x = flat_terrain_2d.size_x / 2
+    map_center_z = flat_terrain_2d.size_z / 2
     print(str(num_circles) + ' circles')
     for i_circle in range(num_circles):
-        r = random.uniform(0, flat_terrain_2d.size / 2)
+        r = random.uniform(0, size / 2)
         num_plants = max(3, random.randint(0, int(r*math.tau)))
         template_name = random.choice(template_names)
         print('circle ' + str(i_circle) + ': radius ' + str(r) + ', ' + str(num_plants) + ' plants')
@@ -109,16 +116,17 @@ def create_plants(flat_terrain_2d: FlatTerrain2D):
             a = math.tau / num_plants * i_plant
             x = math.sin(a)
             z = math.cos(a)
-            map_x = map_center + r*x
-            map_z = map_center + r*z
+            map_x = map_center_x + r*x
+            map_z = map_center_z + r*z
             flat_terrain_2d.plants.append(Plant(template_name, (map_x, map_z)))
 
 
-def create_region(map_name, region_name, size=1):
+def create_region(map_name, region_name, size='4x4'):
     bits = Bits()
     m = bits.maps[map_name]
     region: Region = m.create_region(region_name, None)
-    flat_terrain_2d = FlatTerrain2D(size*4)
+    size_x, size_z = [int(s) for s in size.split('x')]
+    flat_terrain_2d = FlatTerrain2D(size_x, size_z)
     terrain = flat_terrain_2d.make_terrain()
     region.terrain = terrain
     create_plants(flat_terrain_2d)
@@ -128,8 +136,9 @@ def create_region(map_name, region_name, size=1):
     # start positions group
     if len(m.get_regions()) == 1 and m.start_positions is None:
         # 1st region, let's add a start pos
-        map_center = flat_terrain_2d.size / 2
-        position = flat_terrain_2d.map_pos_to_node_pos(map_center, map_center)
+        map_center_x = flat_terrain_2d.size_x / 2
+        map_center_z = flat_terrain_2d.size_z / 2
+        position = flat_terrain_2d.map_pos_to_node_pos(map_center_x, map_center_z)
         m.start_positions = StartPositions({
             'default': StartGroup(
                 'This is the default group.', False, 0, '', [
@@ -156,7 +165,7 @@ def init_arg_parser():
     parser.add_argument('--name')
     parser.add_argument('--screen-name')
     parser.add_argument('--map')
-    parser.add_argument('--size', type=int, default=1)
+    parser.add_argument('--size')
     return parser
 
 
