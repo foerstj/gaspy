ab627430a9b7d5f33114157addc98c06bb97bbcb
Johannes Förstner <johannes@famfoerstner.de>
2021-02-28 22:48:12 +0100

    Tessellation with 4x8 and 8x8 nodes!

--- mapgen.py
+++ mapgen.py
@@ -45,11 +45,16 @@ class Plant:
 
 class FlatTerrain2D:
     class NodeTile:  # 4x4 map square
-        def __init__(self, turned=0):
+        def __init__(self, turned=0, node=None):
             assert 0 <= turned <= 3
-            self.node = None
+            self.node: TerrainNode = node
             self.turned = turned  # number of times node is turned 90° counter-clockwise
-            self.turn_angle = turned * math.tau / 4
+            self.is_target_node = False
+            self.offset = (0, 0)
+            self.doors = None  # NWSE
+
+        def turn_angle(self):
+            return self.turned * math.tau / 4
 
     def __init__(self, size_x=0, size_z=0):
         assert size_x % 4 == 0
@@ -61,7 +66,10 @@ class FlatTerrain2D:
         self.node_size_x = node_size_x
         self.node_size_z = node_size_z
         nodes_2d = [[self.NodeTile(random.randint(0, 3)) for _ in range(node_size_z)] for _ in range(node_size_x)]
-        nodes_2d[int(node_size_x/2)][int(node_size_z/2)].turned = 0  # keep default orientation of target node
+        target_node_tile = nodes_2d[int(node_size_x/2)][int(node_size_z/2)]
+        target_node_tile.turned = 0  # keep default orientation of target node
+        target_node_tile.doors = (1, 2, 3, 4)
+        target_node_tile.is_target_node = True
         self.nodes_2d = nodes_2d
         self.plants: list[Plant] = []
 
@@ -71,28 +79,131 @@ class FlatTerrain2D:
         for x in range(self.node_size_x):
             for z in range(self.node_size_z):
                 node_tile = self.nodes_2d[x][z]
-                node = TerrainNode(terrain.new_node_guid(), 't_xxx_flr_04x04-v0', 'grs01')
+                node = TerrainNode(None, 't_xxx_flr_04x04-v0')
                 node_tile.node = node
-                terrain.nodes.append(node)
+                doors = (1, 2, 3, 4)
+                n = (-node_tile.turned) % 4
+                doors = doors[n:] + doors[:n]
+                node_tile.doors = doors
+        # tessellate
+        if self.node_size_x > 1 and self.node_size_z > 1:
+            num_fails = 0
+            while num_fails < self.node_size_x * self.node_size_z:
+                num_fails += 1
+                x = random.randint(0, self.node_size_x-2)
+                z = random.randint(0, self.node_size_z-2)
+                neighbors = [[self.nodes_2d[x+xx][z+zz] for zz in range(0, 2)] for xx in range(0, 2)]
+                if neighbors[0][0].is_target_node:
+                    continue
+                if neighbors[0][0].node.mesh_name == 't_xxx_flr_04x04-v0':
+                    # try 8x8
+                    if neighbors[0][1].node.mesh_name == 't_xxx_flr_04x04-v0' and neighbors[1][0].node.mesh_name == 't_xxx_flr_04x04-v0' and neighbors[1][1].node.mesh_name == 't_xxx_flr_04x04-v0':
+                        if neighbors[0][1].is_target_node or neighbors[1][0].is_target_node or neighbors[1][1].is_target_node:
+                            continue
+                        node = TerrainNode(None, 't_xxx_flr_08x08-v0')
+                        doors = ((2, 3, None, None), (None, 4, 5, None), (None, None, 6, 7), (1, None, None, 8))
+                        turned = random.randint(0, 3)
+                        n = (-turned) % 4
+                        doors = doors[n:] + doors[:n]
+                        doors = [d[n:] + d[:n] for d in doors]
+                        neighbors[0][0].node = node
+                        neighbors[0][0].turned = turned
+                        turn_angle = neighbors[0][0].turn_angle()
+                        neighbors[0][0].offset = self.turn(-2, -2, -turn_angle)
+                        neighbors[0][0].doors = doors[0]
+                        turn_angle += math.tau/4
+                        neighbors[1][0].node = node
+                        neighbors[1][0].turned = turned
+                        neighbors[1][0].offset = self.turn(-2, -2, -turn_angle)
+                        neighbors[1][0].doors = doors[3]
+                        turn_angle += math.tau/4
+                        neighbors[1][1].node = node
+                        neighbors[1][1].turned = turned
+                        neighbors[1][1].offset = self.turn(-2, -2, -turn_angle)
+                        neighbors[1][1].doors = doors[2]
+                        turn_angle += math.tau/4
+                        neighbors[0][1].node = node
+                        neighbors[0][1].turned = turned
+                        neighbors[0][1].offset = self.turn(-2, -2, -turn_angle)
+                        neighbors[0][1].doors = doors[1]
+                        num_fails = 0
+                    else:  # try 4x8
+                        node = TerrainNode(None, 't_xxx_flr_04x08-v0')
+                        doors = ((2, 3, 4, None), (1, None, 5, 6))
+                        if random.getrandbits(1):
+                            # vertical
+                            if neighbors[0][1].node.mesh_name == 't_xxx_flr_04x04-v0':
+                                if neighbors[0][1].is_target_node:
+                                    continue
+                                turned = 1 if random.getrandbits(1) else 3
+                                n = (-turned) % 4
+                                if turned == 1:
+                                    doors = doors[1:] + doors[:1]
+                                doors = [d[n:] + d[:n] for d in doors]
+                                neighbors[0][0].node = node
+                                neighbors[0][0].turned = turned
+                                turn_angle = neighbors[0][0].turn_angle()
+                                turn_angle += math.tau / 4
+                                neighbors[0][0].offset = self.turn(-2, 0, -turn_angle)
+                                neighbors[0][0].doors = doors[0]
+                                turn_angle += math.tau / 2
+                                neighbors[0][1].node = node
+                                neighbors[0][1].turned = turned
+                                neighbors[0][1].offset = self.turn(-2, 0, -turn_angle)
+                                neighbors[0][1].doors = doors[1]
+                                num_fails = 0
+                        else:
+                            # horizontal
+                            if neighbors[1][0].node.mesh_name == 't_xxx_flr_04x04-v0':
+                                if neighbors[1][0].is_target_node:
+                                    continue
+                                turned = 0 if random.getrandbits(1) else 2
+                                n = (-turned) % 4
+                                if turned == 2:
+                                    doors = doors[1:] + doors[:1]
+                                doors = [d[n:] + d[:n] for d in doors]
+                                neighbors[0][0].node = node
+                                neighbors[0][0].turned = turned
+                                turn_angle = neighbors[0][0].turn_angle()
+                                neighbors[0][0].offset = self.turn(-2, 0, -turn_angle)
+                                neighbors[0][0].doors = doors[0]
+                                turn_angle += math.tau / 2
+                                neighbors[1][0].node = node
+                                neighbors[1][0].turned = turned
+                                neighbors[1][0].offset = self.turn(-2, 0, -turn_angle)
+                                neighbors[1][0].doors = doors[1]
+                                num_fails = 0
+        terrain_nodes = set()
+        for row in self.nodes_2d:
+            for nt in row:
+                terrain_nodes.add(nt.node)
+        for n in terrain_nodes:
+            n.guid = terrain.new_node_guid()
+        terrain.nodes = list(terrain_nodes)
+        # connect
         for x in range(self.node_size_x):
             for z in range(self.node_size_z):
                 nt = self.nodes_2d[x][z]
                 node = nt.node
                 if x > 0:
                     nnt = self.nodes_2d[x-1][z]
-                    node.connect_doors((1 + nt.turned) % 4 + 1, nnt.node, (3 + nnt.turned) % 4 + 1)
+                    node.connect_doors(nt.doors[1], nnt.node, nnt.doors[3])
                 if x < self.node_size_x-1:
                     nnt = self.nodes_2d[x+1][z]
-                    node.connect_doors((3 + nt.turned) % 4 + 1, nnt.node, (1 + nnt.turned) % 4 + 1)
+                    node.connect_doors(nt.doors[3], nnt.node, nnt.doors[1])
                 if z > 0:
                     nnt = self.nodes_2d[x][z-1]
-                    node.connect_doors((0 + nt.turned) % 4 + 1, nnt.node, (2 + nnt.turned) % 4 + 1)
+                    node.connect_doors(nt.doors[0], nnt.node, nnt.doors[2])
                 if z < self.node_size_z-1:
                     nnt = self.nodes_2d[x][z+1]
-                    node.connect_doors((2 + nt.turned) % 4 + 1, nnt.node, (0 + nnt.turned) % 4 + 1)
+                    node.connect_doors(nt.doors[2], nnt.node, nnt.doors[0])
         i_tn_x = int(self.node_size_x / 2)
         i_tn_z = int(self.node_size_z / 2)
         target_nt = self.nodes_2d[i_tn_x][i_tn_z]
+        assert target_nt.node.mesh_name == 't_xxx_flr_04x04-v0'
+        assert target_nt.is_target_node
+        assert target_nt.turned == 0
+        assert target_nt.doors == (1, 2, 3, 4)
         terrain.target_node = target_nt.node
         # ambient light
         terrain.ambient_light.intensity = 0.2
@@ -100,16 +211,23 @@ class FlatTerrain2D:
         terrain.ambient_light.actor_intensity = 0.25
         return terrain
 
+    @staticmethod
+    def turn(x, z, angle):
+        xt = math.cos(angle)*x + math.sin(angle)*z
+        zt = math.cos(angle)*z - math.sin(angle)*x
+        return xt, zt
+
     def map_pos_to_node_pos(self, map_pos_x, map_pos_z):
-        xn = int(map_pos_x / 4)
-        zn = int(map_pos_z / 4)
-        node_tile = self.nodes_2d[xn][zn]
-        nx = map_pos_x % 4 - 2
-        nz = map_pos_z % 4 - 2
-        na = node_tile.turn_angle
-        nxt = math.cos(na)*nx + math.sin(na)*nz
-        nzt = math.cos(na)*nz - math.sin(na)*nx
-        node_pos = Position(nxt, 0, nzt, node_tile.node.guid)
+        xnt = int(map_pos_x / 4)
+        znt = int(map_pos_z / 4)
+        node_tile = self.nodes_2d[xnt][znt]
+        ntx = map_pos_x % 4 - 2
+        ntz = map_pos_z % 4 - 2
+        na = node_tile.turn_angle()
+        ntxt, ntzt = self.turn(ntx, ntz, -na)
+        nx = node_tile.offset[0] + ntxt
+        nz = node_tile.offset[1] + ntzt
+        node_pos = Position(nx, 0, nz, node_tile.node.guid)
         return node_pos
 
     @staticmethod
@@ -123,13 +241,17 @@ class FlatTerrain2D:
             (map_pos_x, map_pos_z) = plant.map_pos
             node_pos = self.map_pos_to_node_pos(map_pos_x, map_pos_z)
             plant.node_pos = node_pos
+            xnt = int(map_pos_x / 4)
+            znt = int(map_pos_z / 4)
+            node_tile = self.nodes_2d[xnt][znt]
+            plant.orientation -= node_tile.turn_angle()
         objects_non_interactive = [(plant.template_name, plant.node_pos, self.rad_to_quat(plant.orientation)) for plant in self.plants]
         return objects_non_interactive
 
 
 def create_plants(flat_terrain_2d: FlatTerrain2D):
     template_names = [
-        'bush_grs_03', 'bush_grs_04', 'bush_grs_05', 'bush_grs_08', 'cornstalk_glb_grn_01', 'fern_grs_01', 'flowers_grs_04', 'flowers_grs_05', 'flowers_grs_06', 'flowers_grs_08', 'flowers_grs_blue',
+        'bush_grs_04', 'bush_grs_05', 'bush_grs_button_01', 'cornstalk_glb_grn_01', 'fern_grs_01', 'flowers_grs_04', 'flowers_grs_05', 'flowers_grs_06', 'flowers_grs_08', 'flowers_grs_blue',
         'foliage_grs_01', 'grass_grs_07', 'groundcover_grs_03', 'mushroom_glb_10', 'mushrooms_cav_06'
     ]
     size = math.sqrt(flat_terrain_2d.size_x*flat_terrain_2d.size_x + flat_terrain_2d.size_z*flat_terrain_2d.size_z)
--- terrain.py
+++ terrain.py
@@ -19,20 +19,28 @@ class AmbientLight:
 
 
 class TerrainNode:
-    def __init__(self, guid, mesh_name, texture_set):
+    def __init__(self, guid, mesh_name, texture_set='grs01'):
         self.guid = guid
         self.mesh_name = mesh_name
         self.texture_set = texture_set
         self.doors: dict[int, (TerrainNode, int)] = dict()
 
     def connect_doors(self, my_door: int, far_node, far_door: int):
-        self.doors[my_door] = (far_node, far_door)
-        far_node.doors[far_door] = (self, my_door)
+        if my_door is None or far_door is None:
+            assert my_door is None and far_door is None
+        elif my_door in self.doors:
+            assert self.doors[my_door] == (far_node, far_door)
+            assert far_node.doors[far_door] == (self, my_door)
+        else:
+            self.doors[my_door] = (far_node, far_door)
+            far_node.doors[far_door] = (self, my_door)
 
 
 class Terrain:
     mesh_index_lookup = {
-        't_xxx_flr_04x04-v0': 0x6a5
+        't_xxx_flr_04x04-v0': 0x6a5,
+        't_xxx_flr_04x08-v0': 0x6a9,
+        't_xxx_flr_08x08-v0': 0x6aa
     }
 
     def __init__(self):
