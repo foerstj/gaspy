7548a9a2d1d61291505180942a64be002c470ed8
Johannes Förstner <johannes@famfoerstner.de>
2021-03-24 18:41:04 +0100

    Delete .lnc files

--- inc_node_ids.py
+++ inc_node_ids.py
@@ -12,8 +12,13 @@ def inc_node_ids(map_name, region_name):
     if region_name is not None:
         region = m.get_region(region_name)
         node_ids = region.get_node_ids()
+        lnc_file = os.path.join(region.gas_dir.get_subdir('terrain_nodes').path, 'siege_nodes.lnc')
+        os.remove(lnc_file)
     else:
         node_ids = m.get_all_node_ids()
+        for region in m.get_regions().values():
+            lnc_file = os.path.join(region.gas_dir.get_subdir('terrain_nodes').path, 'siege_nodes.lnc')
+            os.remove(lnc_file)
     pathlist = Path(m.gas_dir.path).rglob('*.gas')
     for path in pathlist:
         print(path)
