a37df0192564e4afee029d06741d31536647f4a4
Johannes Förstner <johannes@famfoerstner.de>
2021-03-24 18:40:38 +0100

    Enable incrementing a whole map's node ids

--- inc_node_ids.py
+++ inc_node_ids.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -7,25 +8,28 @@ from gas import Hex
 
 def inc_node_ids(map_name, region_name):
     bits = Bits()
-    region = bits.maps[map_name].get_region(region_name)
-    region_dir = region.gas_dir.path
-    node_ids = region.get_node_ids()
-    pathlist = Path(region_dir).rglob('*.gas')
+    m = bits.maps[map_name]
+    if region_name is not None:
+        region = m.get_region(region_name)
+        node_ids = region.get_node_ids()
+    else:
+        node_ids = m.get_all_node_ids()
+    pathlist = Path(m.gas_dir.path).rglob('*.gas')
     for path in pathlist:
         print(path)
-        with open(path) as region_file:
-            text = region_file.read()
+        with open(path) as map_gas_file:
+            text = map_gas_file.read()
         for node_id in node_ids:
             new_node_id = Hex(node_id + 1)
             text = text.replace(node_id.to_str_lower(), new_node_id.to_str_lower())
             text = text.replace(node_id.to_str_upper(), new_node_id.to_str_upper())
-        with open(path, 'w') as region_file:
-            region_file.write(text)
+        with open(path, 'w') as map_gas_file:
+            map_gas_file.write(text)
 
 
 def main(argv):
     map_name = argv[0]
-    region_name = argv[1]
+    region_name = argv[1] if len(argv) > 1 else None
     inc_node_ids(map_name, region_name)
     return 0
 
