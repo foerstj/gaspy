886e98c36d2b2b001d85b1af5adc8c6a1a709b23
Johannes Förstner <johannes@famfoerstner.de>
2021-03-24 17:49:26 +0100

    Extend check to common node ids with other maps

--- check_dupe_node_ids.py
+++ check_dupe_node_ids.py
@@ -9,7 +9,11 @@ def check_dupe_node_ids(map_name):
     node_ids_list = m.get_all_node_ids()
     node_ids = set(node_ids_list)
     assert len(node_ids) == len(node_ids_list), map_name + ' contains duplicate node ids!'
-    # TODO other maps
+    for other_map_name, other_map in bits.maps.items():
+        if other_map_name == map_name:
+            continue
+        other_node_ids = set(other_map.get_all_node_ids())
+        assert node_ids.isdisjoint(other_node_ids), map_name + ' contains common node ids with ' + other_map_name + '!'
     print('All good.')
 
 
