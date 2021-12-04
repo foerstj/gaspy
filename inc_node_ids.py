e159166ea0cfed1c362b4d05774bb598bd33ddd0
Johannes Förstner <johannes@famfoerstner.de>
2021-12-04 13:31:38 +0100

    inc_node_ids: only write file if there are changes

--- inc_node_ids.py
+++ inc_node_ids.py
@@ -9,6 +9,8 @@ from gas import Hex
 def inc_node_ids(map_name, region_name, inc=1):
     bits = Bits()
     m = bits.maps[map_name]
+
+    # step 1: collect node ids and delete the terrain lnc file(s)
     if region_name is not None:
         region = m.get_region(region_name)
         node_ids = region.get_node_ids()
@@ -21,17 +23,22 @@ def inc_node_ids(map_name, region_name, inc=1):
             lnc_file = os.path.join(region.gas_dir.get_subdir('terrain_nodes').path, 'siege_nodes.lnc')
             if os.path.isfile(lnc_file):
                 os.remove(lnc_file)
-    pathlist = Path(m.gas_dir.path).rglob('*.gas')
-    for path in pathlist:
+
+    # step 2: textually replace all node ids in all map files
+    path_list = Path(m.gas_dir.path).rglob('*.gas')
+    for path in path_list:
         print(path)
         with open(path) as map_gas_file:
             text = map_gas_file.read()
+        text_pre = text
         for node_id in node_ids:
             new_node_id = Hex(node_id + inc)
             text = text.replace(node_id.to_str_lower(), new_node_id.to_str_lower())
             text = text.replace(node_id.to_str_upper(), new_node_id.to_str_upper())
-        with open(path, 'w') as map_gas_file:
-            map_gas_file.write(text)
+        if text != text_pre:
+            with open(path, 'w') as map_gas_file:
+                map_gas_file.write(text)
+
     print('Done.')
     print('You have to open & save the converted region(s) in Siege Editor to complete the process.')
 
