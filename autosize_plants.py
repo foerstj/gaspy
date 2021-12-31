f136b2e309aabf6d8bf0263c8762e058a13e183a
Johannes Förstner <johannes@famfoerstner.de>
2021-12-31 14:23:32 +0100

    Add cli params to autosize_plants

--- autosize_plants.py
+++ autosize_plants.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 import sys
 
@@ -5,7 +6,7 @@ from bits import Bits
 from region import GameObject, Region
 
 
-def do_autosize_plants(objects_non_interactive: list[GameObject], overwrite, min_size, max_size, median_size=None):
+def do_autosize_plants(objects_non_interactive: list[GameObject], template_prefix, overwrite, min_size, max_size, median_size=None):
     if median_size is None:
         median_size = (min_size + max_size) / 2
     assert min_size <= median_size <= max_size
@@ -13,7 +14,7 @@ def do_autosize_plants(objects_non_interactive: list[GameObject], overwrite, min
     num_autosized = 0
 
     for i, go in enumerate(objects_non_interactive):
-        if not go.template_name.startswith('tree_'):
+        if not go.template_name.startswith(template_prefix + '_'):
             continue
         num_total += 1
         aspect = go.section.get_or_create_section('aspect')
@@ -32,32 +33,45 @@ def do_autosize_plants(objects_non_interactive: list[GameObject], overwrite, min
     return num_autosized, num_total
 
 
-def autosize_plants_in_region(region: Region):
+def autosize_plants_in_region(region: Region, template_prefix):
     region.load_objects()
-    num_autosized, num_total = do_autosize_plants(region.objects_non_interactive, False, 0.8, 1.3, 1.0)
+    num_autosized, num_total = do_autosize_plants(region.objects_non_interactive, template_prefix, False, 0.8, 1.3, 1.0)
     if num_autosized > 0:
         region.store_objects()
         region.gas_dir.save()
     print(region.get_name() + ': ' + str(num_autosized) + ' / ' + str(num_total) + ' plants autosized')
 
 
-def autosize_plants(map_name, region_name):
-    bits = Bits()
+def autosize_plants(map_name, region_name, plants, bits_dir=None):
+    bits = Bits(bits_dir)
     m = bits.maps[map_name]
     print('autosize plants')
     if region_name:
         region = m.get_region(region_name)
-        autosize_plants_in_region(region)
+        autosize_plants_in_region(region, plants)
     else:
         for region_name, region in m.get_regions().items():
-            autosize_plants_in_region(region)
+            autosize_plants_in_region(region, plants)
     print('autosize plants done')
 
 
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy MapGen')
+    parser.add_argument('map_name')
+    parser.add_argument('region_name', nargs='?', default=None)
+    parser.add_argument('--plants', choices=['tree', 'bush', 'mushroom'], default='tree')
+    parser.add_argument('--bits', default='DSLOA')
+    return parser
+
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
 def main(argv):
-    map_name = argv[0]
-    region_name = argv[1] if len(argv) > 1 else None
-    autosize_plants(map_name, region_name)
+    args = parse_args(argv)
+    autosize_plants(args.map_name, args.region_name, args.plants, args.bits)
     return 0
 
 
--- bits.py
+++ bits.py
@@ -8,9 +8,14 @@ from templates import Templates
 
 
 class Bits(GasDirHandler):
-    def __init__(self, path=None):
-        if path is None:
-            path = os.path.join(os.path.expanduser("~"), 'Documents', 'Dungeon Siege LoA', 'Bits')
+    DSLOA_PATH = os.path.join(os.path.expanduser("~"), 'Documents', 'Dungeon Siege LoA', 'Bits')
+    DS1_PATH = os.path.join(os.path.expanduser("~"), 'Documents', 'Dungeon Siege', 'Bits')
+
+    def __init__(self, path: str = None):
+        if path is None or path.upper() == 'DSLOA':
+            path = Bits.DSLOA_PATH
+        elif path.upper() == 'DS1':
+            path = Bits.DS1_PATH
         assert os.path.isdir(path), path
         super().__init__(GasDir(path))
         self.templates = self.init_templates()
