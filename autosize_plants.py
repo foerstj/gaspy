a5b9a3f2c63d41d5a98e12ea4a83f9e6af892b30
Johannes Förstner <johannes@famfoerstner.de>
2021-12-31 13:56:24 +0100

    Rename autosize_trees to autosize_plants

--- autosize_plants.py
+++ autosize_plants.py
@@ -5,7 +5,7 @@ from bits import Bits
 from region import GameObject, Region
 
 
-def do_autosize_trees(objects_non_interactive: list[GameObject], overwrite, min_size, max_size, median_size=None):
+def do_autosize_plants(objects_non_interactive: list[GameObject], overwrite, min_size, max_size, median_size=None):
     if median_size is None:
         median_size = (min_size + max_size) / 2
     assert min_size <= median_size <= max_size
@@ -32,32 +32,32 @@ def do_autosize_trees(objects_non_interactive: list[GameObject], overwrite, min_
     return num_autosized, num_total
 
 
-def autosize_trees_in_region(region: Region):
+def autosize_plants_in_region(region: Region):
     region.load_objects()
-    num_autosized, num_total = do_autosize_trees(region.objects_non_interactive, False, 0.8, 1.3, 1.0)
+    num_autosized, num_total = do_autosize_plants(region.objects_non_interactive, False, 0.8, 1.3, 1.0)
     if num_autosized > 0:
         region.store_objects()
         region.gas_dir.save()
-    print(region.get_name() + ': ' + str(num_autosized) + ' / ' + str(num_total) + ' trees autosized')
+    print(region.get_name() + ': ' + str(num_autosized) + ' / ' + str(num_total) + ' plants autosized')
 
 
-def autosize_trees(map_name, region_name):
+def autosize_plants(map_name, region_name):
     bits = Bits()
     m = bits.maps[map_name]
-    print('autosize trees')
+    print('autosize plants')
     if region_name:
         region = m.get_region(region_name)
-        autosize_trees_in_region(region)
+        autosize_plants_in_region(region)
     else:
         for region_name, region in m.get_regions().items():
-            autosize_trees_in_region(region)
-    print('autosize trees done')
+            autosize_plants_in_region(region)
+    print('autosize plants done')
 
 
 def main(argv):
     map_name = argv[0]
     region_name = argv[1] if len(argv) > 1 else None
-    autosize_trees(map_name, region_name)
+    autosize_plants(map_name, region_name)
     return 0
 
 
