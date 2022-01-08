6b14f238d0131d282467f62b5ff489af47bd058f
Johannes Förstner <johannes@famfoerstner.de>
2022-01-08 15:29:34 +0100

    Implement kinda dirty backtracking by un-fixing points if there's no match

--- mapgen_heightmap.py
+++ mapgen_heightmap.py
@@ -39,8 +39,115 @@ def gen_perlin_heightmap(tile_size_x, tile_size_z):
     octaves = math.sqrt(max_size_xz) / 2
     print('perlin octaves: ' + str(octaves))
     perlin = PerlinNoise(octaves)
-    heightmap = [[perlin([x/max_size_xz, z/max_size_xz])*2*4*3 for z in range(tile_size_z+1)] for x in range(tile_size_x+1)]
+    heightmap = [[perlin([x/max_size_xz, z/max_size_xz])*2*4*8 for z in range(tile_size_z+1)] for x in range(tile_size_x+1)]
     return heightmap
+
+
+def fit_nodes(tl, tl_fixed, tr, tr_fixed, bl, bl_fixed, br, br_fixed):
+    fixed_heights = [
+        tl if tl_fixed else None,
+        tr if tr_fixed else None,
+        bl if bl_fixed else None,
+        br if br_fixed else None,
+    ]
+    fixed_heights = [p for p in fixed_heights if p is not None]
+    fixed_base = min(fixed_heights)
+
+    # find best-fitting node
+    node_fits = list()
+    nodes = list(NODES.items())
+    random.shuffle(nodes)
+    for mesh, points in nodes:
+        turns = list(range(0, 4))
+        random.shuffle(turns)
+        for turn in turns:
+            turned_points = (points[(0 - turn) % 4], points[(1 - turn) % 4], points[(2 - turn) % 4], points[(3 - turn) % 4])
+            for h in [fixed_base, fixed_base - 4, fixed_base - 8]:
+                tn_tl = turned_points[1] + h
+                tn_tr = turned_points[0] + h
+                tn_bl = turned_points[2] + h
+                tn_br = turned_points[3] + h
+                if tl_fixed and tn_tl != tl:
+                    continue
+                if tr_fixed and tn_tr != tr:
+                    continue
+                if bl_fixed and tn_bl != bl:
+                    continue
+                if br_fixed and tn_br != br:
+                    continue
+                fit = abs(tl - tn_tl) + abs(tr - tn_tr) + abs(bl - tn_bl) + abs(br - tn_br)
+                node_fits.append((mesh, turn, fit, (tn_tl, tn_tr, tn_bl, tn_br)))
+    node_fits.sort(key=lambda x: x[2])  # sort by fit
+    return node_fits[0] if len(node_fits) > 0 else None
+
+
+def gen_tile(tile, tiles, heightmap, tile_size_x, tile_size_z):
+    tl = heightmap[tile.x + 0][tile.z + 0]
+    tr = heightmap[tile.x + 1][tile.z + 0]
+    bl = heightmap[tile.x + 0][tile.z + 1]
+    br = heightmap[tile.x + 1][tile.z + 1]
+
+    tl_fixed = 0
+    tr_fixed = 0
+    bl_fixed = 0
+    br_fixed = 0
+    if tile.x > 0 and tiles[tile.x - 1][tile.z].node_mesh is not None:  # left
+        tl_fixed += 1
+        bl_fixed += 1
+    if tile.x + 1 < tile_size_x and tiles[tile.x + 1][tile.z].node_mesh is not None:  # right
+        tr_fixed += 1
+        br_fixed += 1
+    if tile.z > 0 and tiles[tile.x][tile.z - 1].node_mesh is not None:  # top
+        tl_fixed += 1
+        tr_fixed += 1
+    if tile.z + 1 < tile_size_z and tiles[tile.x][tile.z + 1].node_mesh is not None:  # bottom
+        bl_fixed += 1
+        br_fixed += 1
+    assert tl_fixed or tr_fixed or bl_fixed or br_fixed
+
+    # find best-fitting node
+    best_fit = fit_nodes(tl, tl_fixed, tr, tr_fixed, bl, bl_fixed, br, br_fixed)
+
+    if best_fit is not None:
+        mesh, turn, fit, points = best_fit
+
+        # apply found node
+        tile.node_mesh = mesh
+        tile.node_turn = turn
+        tl, tr, bl, br = points
+        heightmap[tile.x + 0][tile.z + 0] = tl
+        heightmap[tile.x + 1][tile.z + 0] = tr
+        heightmap[tile.x + 0][tile.z + 1] = bl
+        heightmap[tile.x + 1][tile.z + 1] = br
+        return True
+    else:
+        print(repr((tile.x, tile.z)) + ': no fit found for TR-TL-BL-BR ' + repr(((tr, tr_fixed), (tl, tl_fixed), (bl, bl_fixed), (br, br_fixed))))
+        # pick a fixed point and un-fix it by deleting the surrounding nodes
+        fixed_points = [
+            (0, 0, tl_fixed) if tl_fixed else None,
+            (1, 0, tr_fixed) if tr_fixed else None,
+            (0, 1, bl_fixed) if bl_fixed else None,
+            (1, 1, br_fixed) if br_fixed else None,
+        ]
+        fixed_points = [p for p in fixed_points if p is not None]
+        random.shuffle(fixed_points)
+        fixed_points.sort(key=lambda x: x[2])  # sort by fixed (num nodes)
+        x, z, fixed = fixed_points[0]  # avoid unfixing the point with the most nodes
+        print('un-fixing point ' + repr((x, z)))
+
+        x += tile.x
+        z += tile.z
+        if x > 0 and z > 0:
+            tiles[x-1][z-1].node_mesh = None
+        if x < tile_size_x and z > 0:
+            tiles[x-0][z-1].node_mesh = None
+        if x > 0 and z < tile_size_z:
+            tiles[x-1][z-0].node_mesh = None
+        if x < tile_size_x and z < tile_size_z:
+            tiles[x-0][z-0].node_mesh = None
+
+        gen_tile(tile, tiles, heightmap, tile_size_x, tile_size_z)  # try again with fewer constraints
+        return False  # restart to re-generate all deleted tiles
 
 
 def gen_tiles(tile_size_x, tile_size_z, heightmap: list[list[float]]):
@@ -69,78 +176,17 @@ def gen_tiles(tile_size_x, tile_size_z, heightmap: list[list[float]]):
         tile.dist2target = math.sqrt(dx*dx + dz*dz)
     all_tiles.sort(key=lambda x: x.dist2target)
 
-    for tile in all_tiles:
+    i = 0
+    while True:
+        tile = all_tiles[i]
+        i += 1
         if tile.node_mesh is not None:
             continue
-
-        tl = heightmap[tile.x+0][tile.z+0]
-        tr = heightmap[tile.x+1][tile.z+0]
-        bl = heightmap[tile.x+0][tile.z+1]
-        br = heightmap[tile.x+1][tile.z+1]
-
-        tl_fixed = False
-        tr_fixed = False
-        bl_fixed = False
-        br_fixed = False
-        if tile.x > 0 and tiles[tile.x-1][tile.z].node_mesh is not None:
-            tl_fixed = True
-            bl_fixed = True
-        if tile.x+1 < tile_size_x and tiles[tile.x+1][tile.z].node_mesh is not None:
-            tr_fixed = True
-            br_fixed = True
-        if tile.z > 0 and tiles[tile.x][tile.z-1].node_mesh is not None:
-            tl_fixed = True
-            tr_fixed = True
-        if tile.z+1 < tile_size_z and tiles[tile.x][tile.z+1].node_mesh is not None:
-            bl_fixed = True
-            br_fixed = True
-        assert tl_fixed or tr_fixed or bl_fixed or br_fixed
-
-        fixed_heights = [
-            tl if tl_fixed else None,
-            tr if tr_fixed else None,
-            bl if bl_fixed else None,
-            br if br_fixed else None,
-        ]
-        fixed_heights = [p for p in fixed_heights if p is not None]
-        fixed_base = min(fixed_heights)
-
-        # find best-fitting node
-        node_fits = list()
-        nodes = list(NODES.items())
-        random.shuffle(nodes)
-        for mesh, points in nodes:
-            turns = list(range(0, 4))
-            random.shuffle(turns)
-            for turn in turns:
-                turned_points = (points[(0-turn) % 4], points[(1-turn) % 4], points[(2-turn) % 4], points[(3-turn) % 4])
-                for h in [fixed_base, fixed_base-4, fixed_base-8]:
-                    tn_tl = turned_points[1]+h
-                    tn_tr = turned_points[0]+h
-                    tn_bl = turned_points[2]+h
-                    tn_br = turned_points[3]+h
-                    if tl_fixed and tn_tl != tl:
-                        continue
-                    if tr_fixed and tn_tr != tr:
-                        continue
-                    if bl_fixed and tn_bl != bl:
-                        continue
-                    if br_fixed and tn_br != br:
-                        continue
-                    fit = abs(tl - tn_tl) + abs(tr - tn_tr) + abs(bl - tn_bl) + abs(br - tn_br)
-                    node_fits.append((mesh, turn, fit, (tn_tl, tn_tr, tn_bl, tn_br)))
-        assert len(node_fits) > 0, 'no fit found for '+repr(((tr, tr_fixed), (tl, tl_fixed), (br, br_fixed), (bl, bl_fixed)))
-        node_fits.sort(key=lambda x: x[2])  # sort by fit
-        mesh, turn, fit, points = node_fits[0]
-
-        # apply found node
-        tile.node_mesh = mesh
-        tile.node_turn = turn
-        tl, tr, bl, br = points
-        heightmap[tile.x+0][tile.z+0] = tl
-        heightmap[tile.x+1][tile.z+0] = tr
-        heightmap[tile.x+0][tile.z+1] = bl
-        heightmap[tile.x+1][tile.z+1] = br
+        success = gen_tile(tile, tiles, heightmap, tile_size_x, tile_size_z)
+        if not success:
+            i = 0
+        if i >= len(all_tiles):
+            break
 
     return tiles, target_tile
 
