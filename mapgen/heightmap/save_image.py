066f742c55995977283cfa92c488699d3efe3e14
Johannes Förstner <johannes@famfoerstner.de>
2022-02-08 18:42:13 +0100

    Larger plots

--- mapgen/heightmap/save_image.py
+++ mapgen/heightmap/save_image.py
@@ -3,5 +3,6 @@ from matplotlib import pyplot as plt
 
 def save_image(pic: list[list[float]], file_name):
     pic = [[pic[z][x] for z in range(len(pic[x]))] for x in range(len(pic))]  # flip x/z
+    plt.figure(figsize=(len(pic)/256, len(pic[0])/256), dpi=256)
     plt.imshow(pic, cmap='gray')
     plt.savefig(f'output/heightmap/{file_name}.png', bbox_inches='tight')
