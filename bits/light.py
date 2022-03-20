8a667a8e22fcdbe48046328dad1420216fda5274
Johannes Förstner <johannes@famfoerstner.de>
2022-03-20 16:14:48 +0100

    Base class Light

--- bits/light.py
+++ bits/light.py
@@ -3,24 +3,35 @@ import math
 from gas.molecules import Hex
 
 
-class DirectionalLight:
+class Light:
     def __init__(
             self,
-            dl_id: Hex = None,
+            light_id: Hex = None,
             color: Hex = 0xffffffff,
             draw_shadow: bool = False,
             intensity: float = 1,
             occlude_geometry: bool = False,
-            on_timer: bool = False,
-            direction: (float, float, float) = (0, 1, 0)):
-        if dl_id is None:
-            dl_id = Hex.random()
-        self.id = dl_id
+            on_timer: bool = False):
+        if light_id is None:
+            light_id = Hex.random()
+        self.id = light_id
         self.color = color
         self.draw_shadow = draw_shadow
         self.intensity = intensity
         self.occlude_geometry = occlude_geometry
         self.on_timer = on_timer
+        self.active = True
+        self.affects_actors = True
+        self.affects_items = True
+        self.affects_terrain = True
+        self.inner_radius = 0
+        self.outer_radius = 1
+
+
+class DirectionalLight(Light):
+    def __init__(self, dl_id: Hex = None, color: Hex = 0xffffffff, draw_shadow: bool = False, intensity: float = 1, occlude_geometry: bool = False, on_timer: bool = False,
+                 direction: (float, float, float) = (0, 1, 0)):
+        super().__init__(dl_id, color, draw_shadow, intensity, occlude_geometry, on_timer)
         self.direction = direction  # x,y,z vector pointing where the light comes from
 
     @classmethod
