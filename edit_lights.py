4bc6669ff0ee07dfcd9f89dd66e80fa08b921e60
Johannes Förstner <johannes@famfoerstner.de>
2022-03-20 18:21:56 +0100

    Invert hue of all point lights

--- edit_lights.py
+++ edit_lights.py
@@ -1,12 +1,23 @@
 import sys
+import colorsys
 
 from bits.bits import Bits
-from bits.light import Light
+from bits.light import Light, PointLight, Color
 from bits.region import Region
 
 
 def do_edit_region_lights(lights: list[Light]):
-    pass
+    for light in lights:
+        if isinstance(light, PointLight):
+            a, r, g, b = light.color.get_argb()
+            r, g, b = [x/255 for x in (r, g, b)]
+            h, s, v = colorsys.rgb_to_hsv(r, g, b)
+            h += 0.5
+            if h > 1:
+                h -= 1
+            r, g, b = colorsys.hsv_to_rgb(h, s, v)
+            r, g, b = [int(x*255) for x in (r, g, b)]
+            light.color = Color.from_argb(a, r, g, b)
 
 
 def edit_region_lights(region: Region):
