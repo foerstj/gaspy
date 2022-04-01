de6864645a0c140197a8515ac854eded0098fbdb
Johannes Förstner <johannes@famfoerstner.de>
2022-04-01 22:37:02 +0200

    Don't hide 0-xp enemies but _nis_ ones

--- csv.py
+++ csv.py
@@ -111,8 +111,8 @@ def write_enemies_csv(bits: Bits):
     enemies = load_enemies(bits)
 
     enemies = [e for e in enemies if e.screen_name is not None]  # dsx_drake
-    enemies = [e for e in enemies if '_reveal' not in e.template_name and not e.template_name.startswith('test_')]
-    enemies = [e for e in enemies if e.xp]  # enemies with 0 xp aren't included in the wiki either
+    enemies = [e for e in enemies if '_reveal' not in e.template_name and '_nis_' not in e.template_name and not e.template_name.startswith('test_')]
+    # enemies = [e for e in enemies if e.xp]  # enemies with 0 xp aren't included in the wiki either
 
     enemies.sort(key=lambda e: e.screen_name)
     enemies.sort(key=lambda e: e.xp)
