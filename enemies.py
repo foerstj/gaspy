f3d9f5bd68bc41ce7fe376049d6b437ae8659b76
Johannes Förstner <johannes@famfoerstner.de>
2022-04-13 20:33:45 +0200

    Move code into class Enemy and load_enemies

--- enemies.py
+++ enemies.py
@@ -13,9 +13,34 @@ class Enemy:
         self.template = template
         self.template_name = template.name
         self.screen_name: str = template.compute_value('common', 'screen_name')
-        self.xp = int(template.compute_value('aspect', 'experience_value') or '0')
-        self.life = int(template.compute_value('aspect', 'max_life') or '0')
-        self.defense = float(template.compute_value('defend', 'defense') or '0')
+        self.xp = int(template.compute_value('aspect', 'experience_value') or 0)
+        self.life = int(template.compute_value('aspect', 'max_life') or 0)
+        self.defense = float(template.compute_value('defend', 'defense') or 0)
+        self.h2h_min = int(template.compute_value('attack', 'damage_min') or 0)
+        self.h2h_max = int(template.compute_value('attack', 'damage_max') or 0)
+        self.melee_lvl = compute_skill_level(template, 'melee')
+        self.ranged_lvl = compute_skill_level(template, 'ranged')
+        self.magic_lvl = compute_skill_level(template, 'combat_magic')
+        self.strength = compute_skill_level(template, 'strength')
+        self.dexterity = compute_skill_level(template, 'dexterity')
+        self.intelligence = compute_skill_level(template, 'intelligence')
+        self.wpn_pref: str = template.compute_value('mind', 'actor_weapon_preference')
+        icz_melee = template.compute_value('mind', 'on_enemy_entered_icz_switch_to_melee')
+        self.icz_melee = {'true': True, 'false': False}[icz_melee.lower()] if icz_melee else False
+
+    def get_stance(self):
+        stance = self.wpn_pref[3:].capitalize()
+        if self.icz_melee and stance != 'Melee':
+            if self.template.compute_value('inventory', 'selected_active_location'):
+                stance = 'Combo'
+        return stance
+
+    def has_melee_weapon(self):
+        for base_template in self.template.base_templates([self.template]):
+            for inventory in base_template.section.get_sections('inventory'):
+                for _ in inventory.find_attrs_recursive('es_weapon_hand'):
+                    return True
+        return False
 
 
 def load_enemies(bits):
@@ -23,8 +48,9 @@ def load_enemies(bits):
     enemies = [e for n, e in enemies.items() if not (n.startswith('2w_') or n.startswith('3w_'))]
     enemies = [e for e in enemies if 'base' not in e.name]  # unused/forgotten base templates e.g. dsx_base_goblin, dsx_elemental_fire_base
     enemies = [e for e in enemies if 'summon' not in e.name]
-    enemies = [Enemy(e) for e in enemies]
-    return enemies
+    enemies = [e for e in enemies if e.screen_name is not None]  # dsx_drake
+    enemies = [e for e in enemies if '_reveal' not in e.template_name and '_nis_' not in e.template_name and not e.template_name.startswith('test_')]
+    return [Enemy(e) for e in enemies]
 
 
 def compute_skill_level(template: Template, skill: str) -> int:
@@ -42,49 +68,28 @@ def make_enemies_csv_line(enemy: Enemy, extended=False) -> list:
     life = enemy.life
     defense = int(enemy.defense)
     template_name = enemy.template_name
-    wpn_pref: str = enemy.template.compute_value('mind', 'actor_weapon_preference')
-    icz_melee = enemy.template.compute_value('mind', 'on_enemy_entered_icz_switch_to_melee')
-    icz_melee = {'true': True, 'false': False}[icz_melee.lower()] if icz_melee else False
-    stance = wpn_pref[3:].capitalize()
-    if icz_melee and stance != 'Melee':
-        if enemy.template.compute_value('inventory', 'selected_active_location'):
-            stance = 'Combo'
+    stance = enemy.get_stance()
     attacks = []
-    h2h_min = enemy.template.compute_value('attack', 'damage_min') or 0
-    h2h_max = enemy.template.compute_value('attack', 'damage_max') or 0
-    melee_lvl = compute_skill_level(enemy.template, 'melee')
-    ranged_lvl = compute_skill_level(enemy.template, 'ranged')
-    magic_lvl = compute_skill_level(enemy.template, 'combat_magic')
     if stance == 'Magic' or stance == 'Combo':
-        magic_attack = f'(as spell) lvl {magic_lvl}'
+        magic_attack = f'(as spell) lvl {enemy.magic_lvl}'
         attacks.append(magic_attack)
     if stance == 'Melee' or stance == 'Combo':
-        melee_attack_type = 'h2h'
-        for base_template in enemy.template.base_templates([enemy.template]):
-            for inventory in base_template.section.get_sections('inventory'):
-                for _ in inventory.find_attrs_recursive('es_weapon_hand'):
-                    melee_attack_type = '(wpn) +'
-        melee_attack = f'{melee_attack_type} {h2h_min}-{h2h_max} lvl {melee_lvl}'
+        melee_attack_type = 'h2h' if not enemy.has_melee_weapon() else '(wpn) +'
+        melee_attack = f'{melee_attack_type} {enemy.h2h_min}-{enemy.h2h_max} lvl {enemy.melee_lvl}'
         attacks.append(melee_attack)
     if stance == 'Ranged':
-        ranged_attack = f'(wpn) + {h2h_min}-{h2h_max} lvl {ranged_lvl}'
+        ranged_attack = f'(wpn) + {enemy.h2h_min}-{enemy.h2h_max} lvl {enemy.ranged_lvl}'
         attacks.append(ranged_attack)
     attacks = '\n'.join(attacks)
     csv_line = [name, xp, life, defense, stance, attacks, template_name]
     if extended:
-        strength = compute_skill_level(enemy.template, 'strength')
-        dexterity = compute_skill_level(enemy.template, 'dexterity')
-        intelligence = compute_skill_level(enemy.template, 'intelligence')
-        csv_line.extend([wpn_pref, h2h_min, h2h_max, melee_lvl, ranged_lvl, magic_lvl, strength, dexterity, intelligence])
+        csv_line.extend([enemy.wpn_pref, enemy.h2h_min, enemy.h2h_max, enemy.melee_lvl, enemy.ranged_lvl, enemy.magic_lvl, enemy.strength, enemy.dexterity, enemy.intelligence])
     return csv_line
 
 
 # Sth similar to the List of Enemies in the Wiki
 def write_enemies_csv(bits: Bits, extended=False):
     enemies = load_enemies(bits)
-
-    enemies = [e for e in enemies if e.screen_name is not None]  # dsx_drake
-    enemies = [e for e in enemies if '_reveal' not in e.template_name and '_nis_' not in e.template_name and not e.template_name.startswith('test_')]
     if not extended:
         enemies = [e for e in enemies if e.xp]  # enemies with 0 xp aren't included in the wiki either
 
