fc17f3db0464bef866ac695982698a80835dd959
Johannes Förstner <johannes@famfoerstner.de>
2022-08-06 17:21:39 +0200

    Print all SNO details (crashes on desert_canyon\t_dc01_wall-24-tx-dgn-portal-a.sno)

--- bits/snos.py
+++ bits/snos.py
@@ -1,6 +1,8 @@
 import os.path
 from pathlib import Path
 
+from sno.sno_handler import SnoHandler
+
 
 class SNOs:
     def __init__(self, path):
@@ -12,3 +14,5 @@ class SNOs:
         for path in path_list:
             sno_path = os.path.relpath(path, self.path)
             print(sno_path)
+            sno = SnoHandler(path)
+            sno.print('  ')
--- sno/sno_handler.py
+++ sno/sno_handler.py
@@ -14,21 +14,21 @@ class SnoHandler:
         print(f'{indent}texture: {surface.texture}')
 
     @classmethod
-    def print_sno(cls, sno: Sno):
-        print(f'magic: {sno.magic}')
-        print(f'version: {sno.version.major}.{sno.version.minor}')
-        print(f'door count: {sno.door_count}')
-        print(f'spot count: {sno.spot_count}')
-        print(f'corner count: {sno.corner_count}')
-        print(f'face count: {sno.face_count}')
-        print(f'texture count: {sno.texture_count}')
-        print(f'bounding box: {cls.v3_str(sno.bounding_box.min)} - {cls.v3_str(sno.bounding_box.max)}')
-        print(f'unk 1-7: {sno.unk1} {sno.unk2} {sno.unk3} {sno.unk4} {sno.unk5} {sno.unk6} {sno.unk7}')
-        print(f'mystery section count: {sno.mystery_section_count}')
-        print(f'checksum: {sno.checksum}')
-        print(f'surfaces:')
+    def print_sno(cls, sno: Sno, indent=''):
+        print(f'{indent}magic: {sno.magic}')
+        print(f'{indent}version: {sno.version.major}.{sno.version.minor}')
+        print(f'{indent}door count: {sno.door_count}')
+        print(f'{indent}spot count: {sno.spot_count}')
+        print(f'{indent}corner count: {sno.corner_count}')
+        print(f'{indent}face count: {sno.face_count}')
+        print(f'{indent}texture count: {sno.texture_count}')
+        print(f'{indent}bounding box: {cls.v3_str(sno.bounding_box.min)} - {cls.v3_str(sno.bounding_box.max)}')
+        print(f'{indent}unk 1-7: {sno.unk1} {sno.unk2} {sno.unk3} {sno.unk4} {sno.unk5} {sno.unk6} {sno.unk7}')
+        print(f'{indent}mystery section count: {sno.mystery_section_count}')
+        print(f'{indent}checksum: {sno.checksum}')
+        print(f'{indent}surfaces:')
         for surface in sno.surface_array:
-            cls.print_surface(surface, '  ')
+            cls.print_surface(surface, indent + '  ')
 
-    def print(self):
-        self.print_sno(self.sno)
+    def print(self, indent=''):
+        self.print_sno(self.sno, indent)
