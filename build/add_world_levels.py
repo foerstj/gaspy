d1c77ad8846787aacc395b7c03f59d2ee5f44faf
Johannes Förstner <johannes@famfoerstner.de>
2022-08-14 17:07:47 +0200

    Enable using different Bits for map vs. templates

--- build/add_world_levels.py
+++ build/add_world_levels.py
@@ -4,16 +4,18 @@ from bits.bits import Bits
 from world_levels import add_map_world_levels
 
 
-def add_world_levels(bits: Bits, map_name: str):
-    _map = bits.maps[map_name]
-    add_map_world_levels(_map, bits)
+def add_world_levels(map_bits: Bits, map_name: str, template_bits: Bits):
+    _map = map_bits.maps[map_name]
+    add_map_world_levels(_map, template_bits)
 
 
 def main(argv):
     map_name = argv[0]
-    bits_path = argv[1] if len(argv) > 1 else None
-    bits = Bits(bits_path)
-    add_world_levels(bits, map_name)
+    map_bits_path = argv[1] if len(argv) > 1 else None
+    template_bits_path = argv[2] if len(argv) > 2 else None
+    map_bits = Bits(map_bits_path)
+    template_bits = Bits(template_bits_path) if template_bits_path is not None else map_bits
+    add_world_levels(map_bits, map_name, template_bits)
 
 
 if __name__ == '__main__':
