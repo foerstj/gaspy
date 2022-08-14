a86b7b8b2b4b053a05dec9ba3de15d421179b947
Johannes Förstner <johannes@famfoerstner.de>
2022-08-14 15:36:13 +0200

    Print out shrine values compared across world levels

--- csv.py
+++ csv.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from bits.bits import Bits
+from bits.map import Map
 from gas.gas_parser import GasParser
 from bits.templates import Template
 
@@ -303,9 +304,43 @@ def write_level_enemies_csv(bits: Bits):
     write_csv('Enemies Level Chart', data)
 
 
+class Shrine:
+    def __init__(self, scid: str, region_name: str, data: dict):
+        self.scid = scid
+        self.region_name = region_name
+        self.data = data
+
+
+def print_world_level_shrines(_map: Map):
+    shrines = {}
+    for region in _map.get_regions().values():
+        objs_dir = region.gas_dir.get_subdir('objects')
+        for wl in ['regular', 'veteran', 'elite']:
+            wl_dir = objs_dir.get_subdir(wl)
+            gas_file = wl_dir.get_gas_file('special')
+            for go in gas_file.get_gas().get_sections():
+                t, n = go.get_t_n_header()
+                if t != 'life_shrine':  # life_shrine / mana_shrine
+                    continue
+                if n not in shrines:
+                    shrines[n] = Shrine(n, region.get_name(), {'regular': None, 'veteran': None, 'elite': None})
+                f = go.get_section('fountain')
+                shrines[n].data[wl] = {'heal_amount': f.get_attr_value('heal_amount'), 'health_left': f.get_attr_value('health_left'), 'health_regen': f.get_attr_value('health_regen')}
+    shrines_list: list[Shrine] = sorted(shrines.values(), key=lambda x: x.data['regular']['heal_amount'])
+    csv = [['SCID', 'Region', 'heal_amount regular', 'heal_amount veteran', 'heal_amount elite', 'health_left regular', 'health_left veteran', 'health_left elite', 'health_regen regular', 'health_regen veteran', 'health_regen elite']]
+    for shrine in shrines_list:
+        r, v, e = shrine.data['regular'], shrine.data['veteran'], shrine.data['elite']
+        ar, lr, rr = r['heal_amount'], r['health_left'], r['health_regen']
+        av, lv, rv = v['heal_amount'], v['health_left'], v['health_regen']
+        ae, le, re = e['heal_amount'], e['health_left'], e['health_regen']
+        print(f'{shrine.scid}: heal_amount {ar}/{av}/{ae}, health_left {lr}/{lv}/{le}, health_regen {rr}/{rv}/{re}  ({shrine.region_name})')
+        csv.append([shrine.scid, shrine.region_name, ar, av, ae, lr, lv, le, rr, rv, re])
+    write_csv('World-Level Shrines', csv)
+
+
 def init_arg_parser():
     parser = argparse.ArgumentParser(description='GasPy CSV')
-    parser.add_argument('which', choices=['level-enemies', 'enemy-occurrence', 'enemies', 'map-levels'])
+    parser.add_argument('which', choices=['level-enemies', 'enemy-occurrence', 'enemies', 'map-levels', 'world-level-shrines'])
     parser.add_argument('--bits', default=None)
     parser.add_argument('--map-name', nargs='?')
     return parser
@@ -330,6 +365,11 @@ def main(argv):
         assert map_name
         assert map_name in bits.maps
         write_map_levels_csv(bits.maps[map_name])
+    elif which == 'world-level-shrines':
+        map_name = args.map_name
+        assert map_name
+        assert map_name in bits.maps
+        print_world_level_shrines(bits.maps[map_name])
     return 0
 
 
