21c8ea5238c16510975bc2bff3a65cad26fa4304
Johannes Förstner <johannes@famfoerstner.de>
2022-08-21 22:17:18 +0200

    Handle existing template triggers better

--- valhalla.py
+++ valhalla.py
@@ -17,36 +17,52 @@ def scale_enemy(template: Template):
     template.section.get_or_create_section('aspect').set_attr_value('scale_base', scale_base)
 
 
-def has_template_triggers(template: Template):
+def get_own_template_triggers(template: Template):
+    template_triggers = None
     for common in template.section.get_sections('common'):
         tts = common.get_section('template_triggers')
         if tts is not None:
-            return len(tts.items) > 0
+            template_triggers = tts  # last one wins
+    return template_triggers
+
+
+def get_template_triggers(template: Template):
+    template_triggers = get_own_template_triggers(template)
+    if template_triggers is not None:
+        return template_triggers
     if template.parent_template is not None:
-        return has_template_triggers(template.parent_template)
-    return False
+        return get_template_triggers(template.parent_template)
+    return None
 
 
 def bling_enemy(template: Template):
     name = template.name.lower()
     print(name)
     if name.startswith('2w_') or name.startswith('3w_'):
-        if not has_template_triggers(template):
-            random.seed(name)  # random but reproducible
-            color = random.choice(['red', 'green', 'blue', 'yellow', 'cyan', 'purple'])
-            sfxs = ['unique_light_'+color]
-            if name.startswith('3w_'):
-                sfxs.append('unique_ray_'+color)
-            commons = template.section.get_sections('common')
-            common = commons[0] if len(commons) > 0 else template.section.get_or_create_section('common')
-            for sfx in sfxs:
-                trigger = Section('*', [
-                    Attribute('condition*', 'receive_world_message("WE_ENTERED_WORLD")'),
-                    Attribute('action*', f'call_sfx_script("{sfx}(sgx)")'),
-                    Attribute('no_trig_bits', True),
-                    Attribute('single_shot', True)
-                ])
-                common.get_or_create_section('template_triggers').insert_item(trigger)
+        template_triggers = get_template_triggers(template)
+        own_template_triggers = get_own_template_triggers(template)
+        commons = template.section.get_sections('common')
+        common = commons[0] if len(commons) > 0 else template.section.get_or_create_section('common')
+        if own_template_triggers is None:
+            if template_triggers is None:
+                own_template_triggers = Section('template_triggers')
+            else:
+                own_template_triggers = template_triggers.copy()  # copy base template's triggers if necessary
+            common.insert_item(own_template_triggers)
+
+        random.seed(name)  # random but reproducible
+        color = random.choice(['red', 'green', 'blue', 'yellow', 'cyan', 'purple'])
+        sfxs = ['unique_light_'+color]
+        if name.startswith('3w_'):
+            sfxs.append('unique_ray_'+color)
+        for sfx in sfxs:
+            trigger = Section('*', [
+                Attribute('condition*', 'receive_world_message("WE_ENTERED_WORLD")'),
+                Attribute('action*', f'call_sfx_script("{sfx}(sgx)")'),
+                Attribute('no_trig_bits', True),
+                Attribute('single_shot', True)
+            ])
+            own_template_triggers.insert_item(trigger)
 
 
 def pimp_enemy(template: Template):
