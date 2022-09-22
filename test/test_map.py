3197c1fba016e4aa1450e191b25a28be953054f2
Johannes Förstner <johannes@famfoerstner.de>
2022-09-22 17:51:40 +0200

    Use testing Bits dir within project dir

--- test/test_map.py
+++ test/test_map.py
@@ -7,8 +7,16 @@ from bits.map import Map
 
 
 class TestMap(unittest.TestCase):
+    extracts_dir = os.path.join('files', 'extracts')
+    assert os.path.isdir(extracts_dir)
+    maps_bits_dir = os.path.join(extracts_dir, 'Maps')
+    assert os.path.isdir(maps_bits_dir)
+
+    bits_dir = os.path.join('files', 'Bits')
+    assert os.path.isdir(bits_dir)
+
     def test_koe(self):
-        bits = Bits()
+        bits = Bits(self.maps_bits_dir)
         map_dir = bits.gas_dir.get_subdir('world').get_subdir('maps').get_subdir('map_world')
         m = Map(map_dir, bits)
         self.assertIsInstance(m, Map)
@@ -17,14 +25,14 @@ class TestMap(unittest.TestCase):
         self.assertEqual(81, len(m.get_regions()))
 
     def test_init_new(self):
-        bits = Bits()
+        bits = Bits(self.bits_dir)
         map_dir_path = os.path.join(bits.gas_dir.path, 'world', 'maps', 'gaspy-unit-test-map')
         self.assertFalse(os.path.exists(map_dir_path))
         m = Map(GasDir(map_dir_path), bits)
         self.assertIsInstance(m, Map)
 
     def test_save_and_delete(self):
-        bits = Bits()
+        bits = Bits(self.bits_dir)
         map_dir_path = os.path.join(bits.gas_dir.path, 'world', 'maps', 'gaspy-unit-test-map')
         self.assertFalse(os.path.exists(map_dir_path))
         m = Map(GasDir(map_dir_path), bits, Map.Data(name='gaspy-unit-test-map', screen_name='GasPy UnitTest Map!'))
