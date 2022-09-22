3d6237964577c580a015ac2a2468c1e382c483b5
Johannes Förstner <johannes@famfoerstner.de>
2022-09-22 16:23:31 +0200

    Check quest names in conversations

--- bits/conversations.py
+++ bits/conversations.py
@@ -5,19 +5,24 @@ from gas.gas_file import GasFile
 
 
 class ConversationItem:
-    def __init__(self, screen_text: str = None):
+    def __init__(self, screen_text: str = None, activate_quests: list[str] = None, complete_quests: list[str] = None):
         self.screen_text = screen_text
+        self.activate_quests = activate_quests or list()  # list of "quest_name(,i)"
+        self.complete_quests = complete_quests or list()  # list of quest names
 
     @classmethod
     def from_gas(cls, item_section: Section) -> ConversationItem:
         assert item_section.header == 'text*'
         screen_text = item_section.get_attr_value('screen_text')
-        return ConversationItem(screen_text)
+        activate_quests = [a.value for a in item_section.get_attrs('activate_quest*')]
+        complete_quests = [a.value for a in item_section.get_attrs('complete_quest*')]
+        return ConversationItem(screen_text, activate_quests, complete_quests)
 
+    # Not fit for use - fields incomplete!
     def to_gas(self) -> Section:
         return Section('text*', [
             Attribute('screen_text', self.screen_text)
-        ])
+        ] + [Attribute('activate_quest*', quest_name) for quest_name in self.activate_quests] + [Attribute('complete_quest*', quest_name) for quest_name in self.complete_quests])
 
 
 class ConversationsGas:
@@ -33,6 +38,7 @@ class ConversationsGas:
             conversations[conversation_name] = conversation_items
         return ConversationsGas(conversations)
 
+    # Not fit for use - fields incomplete!
     def write_gas(self) -> Gas:
         return Gas([
             Section(name, [item.to_gas() for item in items]) for name, items in self.conversations.items()
