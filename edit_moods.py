a7eb40b2ff08d546911fb6ee73ead4bfc0b8c8ef
Johannes Förstner <johannes@famfoerstner.de>
2022-09-25 12:15:24 +0200

    --edit fog:near-dists:half

--- edit_moods.py
+++ edit_moods.py
@@ -2,16 +2,41 @@ import argparse
 import sys
 
 from bits.bits import Bits
+from bits.moods import Moods
 
 
-def edit_moods(bits_path: str):
+def half_float(value):
+    if value is None:
+        return None
+    if isinstance(value, str):
+        value = float(value.rstrip('f'))
+    return float(value) / 2
+
+
+def edit_fog(moods: Moods, edit: list[str]):
+    if edit == ['near-dists', 'half']:
+        for mood in moods.get_all_moods():
+            mood.fog.near_dist = half_float(mood.fog.near_dist)
+            mood.fog.lowdetail_near_dist = half_float(mood.fog.lowdetail_near_dist)
+
+
+def do_edit_moods(moods: Moods, edit: str):
+    edit = edit.split(':')
+    if edit[0] == 'fog':
+        edit_fog(moods, edit[1:])
+
+
+def edit_moods(bits_path: str, edits: list[str]):
     bits = Bits(bits_path)
     bits.moods.load_moods()
+    for edit in edits:
+        do_edit_moods(bits.moods, edit)
     bits.moods.save()
 
 
 def init_arg_parser():
     parser = argparse.ArgumentParser(description='GasPy edit moods')
+    parser.add_argument('--edit', action='append')
     parser.add_argument('--bits', default=None)
     return parser
 
@@ -23,7 +48,7 @@ def parse_args(argv):
 
 def main(argv):
     args = parse_args(argv)
-    edit_moods(args.bits)
+    edit_moods(args.bits, args.edit or [])
 
 
 if __name__ == '__main__':
