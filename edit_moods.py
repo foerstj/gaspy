5ebebe9996608f7eea71482dbdc1e8f3dd953d18
Johannes Förstner <johannes@famfoerstner.de>
2022-09-25 09:11:17 +0200

    Begin script to edit moods

---
+++ edit_moods.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+
+from bits.bits import Bits
+
+
+def edit_moods(bits_path: str):
+    bits = Bits(bits_path)
+    print(f'Edit moods in {bits.gas_dir.path}')
+
+
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy edit moods')
+    parser.add_argument('--bits', default=None)
+    return parser
+
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
+    edit_moods(args.bits)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
