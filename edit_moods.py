f10af9a4e4bc13973a517e4d68869084ba9cd547
Johannes Förstner <johannes@famfoerstner.de>
2022-09-25 09:42:48 +0200

    Recurse over mood files & subdirs

--- edit_moods.py
+++ edit_moods.py
@@ -2,11 +2,25 @@ import argparse
 import sys
 
 from bits.bits import Bits
+from gas.gas_dir import GasDir
+from gas.gas_file import GasFile
+
+
+def edit_moods_file(gas_file: GasFile):
+    print(gas_file.path)
+
+
+def edit_moods_dir(gas_dir: GasDir):
+    for gas_file in gas_dir.get_gas_files().values():
+        edit_moods_file(gas_file)
+    for subdir in gas_dir.get_subdirs().values():
+        edit_moods_dir(subdir)  # recurse
 
 
 def edit_moods(bits_path: str):
     bits = Bits(bits_path)
     print(f'Edit moods in {bits.gas_dir.path}')
+    edit_moods_dir(bits.moods.gas_dir)
 
 
 def init_arg_parser():
