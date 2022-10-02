ed9e4e457a1da2739678b9021e26148a9ed97c51
Johannes Förstner <johannes@famfoerstner.de>
2022-10-02 14:18:18 +0200

    Use same param for node ids increment

--- region_import/copy_region.py
+++ region_import/copy_region.py
@@ -19,6 +19,7 @@ def inc_region_node_ids(region: Region, inc=1):
 
     # step 2: textually replace all node ids in all region files
     hexes = [(node_id, Hex(node_id + inc)) for node_id in node_ids]
+    # todo warn/fail if an id exists
     replace_hexes_in_dir(region.gas_dir.path, hexes)
 
 
@@ -40,7 +41,7 @@ def copy_region(map_name, old_region_name, new_region_name, inc_ids=1):
     edit_region_scid_range(new_region, Hex(old_region.get_data().scid_range + inc_ids), isolated=True)
     edit_region_mesh_range(new_region, Hex(old_region.get_data().mesh_range + inc_ids))  # technically not necessary but for good measure...
     print('Incrementing node ids of new region...')
-    inc_region_node_ids(new_region)
+    inc_region_node_ids(new_region, inc_ids)
 
     new_region.print()
     print('Copying region done. Recommend to git-commit, then open & save in Siege Editor.')
