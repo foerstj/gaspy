6ef1722ed817e185dd6065c7d10ea0e26c1a6cf5
Johannes Förstner <johannes@famfoerstner.de>
2022-10-12 16:24:04 +0200

    Add prefixes to doc & category name

--- world_level_templates.py
+++ world_level_templates.py
@@ -36,14 +36,31 @@ def adapt_wl_template(section: Section, wl_prefix: str, file_name: str):
         # ignore rogue components after template accidentally closed with one too many brackets
         print(f'Warning: non-template section [{section.header}] in file {file_name}')
         return
+
+    # template name in header
     t, n = section.get_t_n_header()
     assert t == 'template'
     n = f'{wl_prefix}_{n}'
     section.set_t_n_header(t, n)
 
+    # base template name
     specializes_attr = section.get_attr('specializes')
     if specializes_attr is not None:
         specializes_attr.set_value(f'{wl_prefix}_{specializes_attr.value}')
+
+    # doc & category
+    doc_attrs = section.get_attrs('doc')
+    for doc_attr in doc_attrs:
+        doc = doc_attr.value.strip('"')
+        doc = f'"{wl_prefix}_{doc}"'
+        doc_attr.set_value(doc)
+    category_attrs = section.get_attrs('category_name')
+    for category_attr in category_attrs:
+        category = category_attr.value.strip('"')
+        if category.lower().startswith('1w_'):
+            category = category[3:]
+        category = f'"{wl_prefix}_{category}"'
+        category_attr.set_value(category)
 
 
 def adapt_wl_template_file(gas_file: GasFile, wl: str, wl_prefix: str):
