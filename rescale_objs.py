d1ffea84cd08b1639ac885831b0d335b6cd45799
Johannes Förstner <johannes@famfoerstner.de>
2022-10-18 17:44:14 +0200

    Also rescale actor & interactive objects

--- rescale_objs.py
+++ rescale_objs.py
@@ -1,5 +1,7 @@
 # - Use rescaled versions of templates (using scale_base instead of scale_multiplier)
 # - Adapt object scale_multiplier values so that size is unchanged
+# Rescaled templates are defined in minibits: https://github.com/foerstj/minibits/tree/main/templates-rescaled
+
 import sys
 
 from bits.bits import Bits
@@ -12,24 +14,26 @@ def rescale_objs_region(region: Region, misscaled_templates: dict[str, Template]
     print(region.get_name())
     region.objects.load_objects()
     num_rescaled = 0
-    for go in region.objects.objects_non_interactive:
-        assert isinstance(go, GameObject)
-        if go.template_name in misscaled_templates:
-            num_rescaled += 1
-            rescaled_template_name = go.template_name + '_rescaled'
-            go.section.set_t_n_header(rescaled_template_name, go.section.get_t_n_header()[1])
-            scale_multiplier = misscaled_templates[go.template_name].compute_value('aspect', 'scale_multiplier')
-            assert scale_multiplier is not None
-            scale_multiplier = float(scale_multiplier)
-            assert scale_multiplier > 0
-            scale_multiplier = 1 / scale_multiplier
-            scale_multiplier_attr = go.section.get_or_create_section('aspect').get_attr('scale_multiplier')
-            if scale_multiplier_attr:
-                scale_multiplier *= float(scale_multiplier_attr.value)
-            go.section.get_section('aspect').set_attr_value('scale_multiplier', scale_multiplier)
+    for gos in [region.objects.objects_actor, region.objects.objects_interactive, region.objects.objects_non_interactive]:
+        for go in gos:
+            assert isinstance(go, GameObject)
+            if go.template_name in misscaled_templates:
+                num_rescaled += 1
+                rescaled_template_name = go.template_name + '_rescaled'
+                go.section.set_t_n_header(rescaled_template_name, go.section.get_t_n_header()[1])
+                scale_multiplier = misscaled_templates[go.template_name].compute_value('aspect', 'scale_multiplier')
+                assert scale_multiplier is not None
+                scale_multiplier = float(scale_multiplier)
+                assert scale_multiplier > 0
+                scale_multiplier = 1 / scale_multiplier
+                scale_multiplier_attr = go.section.get_or_create_section('aspect').get_attr('scale_multiplier')
+                if scale_multiplier_attr:
+                    scale_multiplier *= float(scale_multiplier_attr.value)
+                go.section.get_section('aspect').set_attr_value('scale_multiplier', scale_multiplier)
     if num_rescaled:
         print(f'  {num_rescaled} objects rescaled')
         region.save()
+    return num_rescaled
 
 
 def get_misscaled_templates(bits: Bits) -> dict[str, Template]:
@@ -49,8 +53,10 @@ def rescale_objs(map_name: str, region_name: str):
     if region_name is not None:
         rescale_objs_region(m.get_region(region_name), misscaled_templates)
     else:
+        num_rescaled = 0
         for region in m.get_regions().values():
-            rescale_objs_region(region, misscaled_templates)
+            num_rescaled += rescale_objs_region(region, misscaled_templates)
+        print(f'Altogether {num_rescaled} objects rescaled')
 
 
 def main(argv):
