2cf8ae2e83b44509449d6a8d2ca7cd7633f40460
Johannes Förstner <johannes@famfoerstner.de>
2022-10-29 20:45:38 +0200

    Randomize decal placement some more

--- landscaping/unpath.py
+++ landscaping/unpath.py
@@ -12,29 +12,36 @@ from gas.molecules import Position
 def unpath_region(region: Region):
     print(region.get_name())
     region.load_terrain()
-    changes = 0
+    num_changed_nodes = 0
+    num_added_decals = 0
+    sizes = {'04x04': 1, '08x04': 2, '08x08': 4}
     for node in region.terrain.nodes:
         if 'pth' not in node.mesh_name:
             continue
         changed = False
-        for size in '04x04', '08x04', '08x08':
+        for size in sizes:
             if node.mesh_name.startswith(f't_xxx_pth_{size}-'):
                 changed = True
                 flr_size = size if size != '08x04' else '04x08'  # sigh
                 node.mesh_name = f't_xxx_flr_{flr_size}-v0'
-                # add decal
-                decal_texture = f'art\\bitmaps\\decals\\b_d_{node.texture_set}-path.raw'  # naming convention established by minibits/generic-decals
-                decal_origin = Position(random.uniform(-2, 2), 1, random.uniform(-2, 2), node.guid)
-                decal_orientation = Decal.rad_to_decal_orientation(random.uniform(0, math.tau))
-                region.get_decals().decals.append(Decal(texture=decal_texture, decal_origin=decal_origin, decal_orientation=decal_orientation))
+                # add decals
+                for _ in range(sizes[size]):
+                    if random.uniform(0, 1) > 0.5:
+                        continue
+                    decal_texture = f'art\\bitmaps\\decals\\b_d_{node.texture_set}-path.raw'  # naming convention established by minibits/generic-decals
+                    decal_origin = Position(random.uniform(-2, 2), 1, random.uniform(-2, 2), node.guid)
+                    decal_orientation = Decal.rad_to_decal_orientation(random.uniform(0, math.tau))
+                    region.get_decals().decals.append(Decal(texture=decal_texture, decal_origin=decal_origin, decal_orientation=decal_orientation))
+                    num_added_decals += 1
         if changed:
-            changes += 1
+            num_changed_nodes += 1
         else:
             print(f'Warning: unhandled path node: {node.mesh_name} {node.guid}')
-    if changes:
-        print(f'Converted {changes} nodes')
+    if num_changed_nodes:
+        print(f'Converted {num_changed_nodes} nodes, added {num_added_decals} decals')
         region.save()
-        region.delete_lnc()
+        if num_added_decals:
+            region.delete_lnc()
 
 
 def unpath(bits_path: str, map_name: str, region_name: str):
