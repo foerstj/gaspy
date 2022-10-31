a19b972475e265d2707879dfa09f9559786fba04
Johannes Förstner <johannes@famfoerstner.de>
2022-10-31 10:18:24 +0100

    Print replacement counts

--- landscaping/replace_objs.py
+++ landscaping/replace_objs.py
@@ -6,9 +6,15 @@ from bits.maps.game_object import GameObject
 from bits.maps.region import Region
 
 
-def replace_objs_in_region(region: Region, replacements: dict[str, str]):
+def print_changes(name: str, changes: dict[str, int], print_all=False):
+    changes_strs = [f'{c} {t}' for t, c in changes.items() if c or print_all]
+    changes_str = ', '.join(changes_strs) if len(changes_strs) else 'none'
+    print(f'{name} replacements: {changes_str}')
+
+
+def replace_objs_in_region(region: Region, replacements: dict[str, str]) -> dict[str, int]:
     region.objects.load_objects()
-    changes = 0
+    changes = {t: 0 for t in replacements}
     for objs in [region.objects.objects_actor, region.objects.objects_interactive, region.objects.objects_non_interactive]:
         for obj in objs:
             assert isinstance(obj, GameObject)
@@ -16,21 +22,32 @@ def replace_objs_in_region(region: Region, replacements: dict[str, str]):
                 t, n = obj.section.get_t_n_header()
                 t = replacements[obj.template_name]
                 obj.section.set_t_n_header(t, n)
-                changes += 1
-    if changes:
+                changes[obj.template_name] += 1
+    if sum(changes.values()):
         region.save()
+        print_changes(region.get_name(), changes)
+    return changes
+
+
+def combine_changes(changes: dict[str, int], new_changes: dict[str, int]):
+    for t in new_changes:
+        changes[t] += new_changes[t]
 
 
 def replace_objs(bits_path: str, map_name: str, region_names: list[str], replacements: dict[str, str]):
     bits = Bits(bits_path)
     m = bits.maps[map_name]
 
+    changes = {t: 0 for t in replacements}
     if len(region_names) > 0:
         for region_name in region_names:
-            replace_objs_in_region(m.get_region(region_name), replacements)
+            region_changes = replace_objs_in_region(m.get_region(region_name), replacements)
+            combine_changes(changes, region_changes)
     else:
         for region in m.get_regions().values():
-            replace_objs_in_region(region, replacements)
+            region_changes = replace_objs_in_region(region, replacements)
+            combine_changes(changes, region_changes)
+    print_changes(m.get_name(), changes)
 
 
 def init_arg_parser():
