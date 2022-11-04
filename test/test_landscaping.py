b2a1ca812da1661b983cce8ae4d0657d958701bf
Johannes Förstner <johannes@famfoerstner.de>
2022-11-04 18:51:15 +0100

    Fix unit test

--- test/test_landscaping.py
+++ test/test_landscaping.py
@@ -11,6 +11,6 @@ class TestMap(unittest.TestCase):
 
     def test_ruinate(self):
         self.files.copy_map_region('map_world', 'path2crypts')  # map_world has no world-level sub-folders; path2crypts has both signs and torches
-        ruinate.ruinate(self.files.bits_dir, 'map_world', ['path2crypts'], Namespace(signs='remove', torches='unlit'))
+        ruinate.ruinate(self.files.bits_dir, 'map_world', ['path2crypts'], Namespace(signs='remove', torches='unlit', lamp_posts=None, candles=None, furniture=None))
         self.files.cleanup_map('map_world')
         # just happy if it doesn't crash tbh
