ac3f83bc659785f42ab84d32421a9000d924c430
Johannes Förstner <johannes@famfoerstner.de>
2022-12-18 20:33:23 +0100

    Improve output of check_moods

--- build/check_moods.py
+++ build/check_moods.py
@@ -4,8 +4,8 @@ from bits.bits import Bits
 from bits.maps.region import Region
 
 
-def check_moods_in_region(region: Region, mood_names: list[str]) -> int:
-    num_invalid_mood_names = 0
+def check_moods_in_region(region: Region, mood_names: list[str]) -> set[str]:
+    invalid_mood_names = set()
     special_objects = region.objects.do_load_objects_special() or list()
     for obj in special_objects:
         common = obj.section.get_section('common')
@@ -21,21 +21,21 @@ def check_moods_in_region(region: Region, mood_names: list[str]) -> int:
                 if attr.value.startswith('mood_change'):
                     mood_name = attr.value.split('"')[1].strip()
                     if mood_name not in mood_names:
-                        print(f'Invalid mood name in {region.get_name()}: {mood_name}')
-                        num_invalid_mood_names += 1
-    return num_invalid_mood_names
+                        print(f'Invalid mood name in {region.get_name()} {obj.object_id}: {mood_name}')
+                        invalid_mood_names.add(mood_name)
+    return invalid_mood_names
 
 
 def check_moods(bits: Bits, map_name: str) -> bool:
     _map = bits.maps[map_name]
     mood_names = bits.moods.get_mood_names()
-    num_invalid_mood_names = 0
+    invalid_mood_names = set()
     print(f'Checking moods in {map_name}...')
     for region in _map.get_regions().values():
         region_invalid_mood_names = check_moods_in_region(region, mood_names)
-        num_invalid_mood_names += region_invalid_mood_names
-    print(f'Checking moods in {map_name}: {num_invalid_mood_names} invalid mood names')
-    return num_invalid_mood_names == 0
+        invalid_mood_names.update(region_invalid_mood_names)
+    print(f'Checking moods in {map_name}: {len(invalid_mood_names)} distinct invalid mood names')
+    return len(invalid_mood_names) == 0
 
 
 def main(argv) -> int:
