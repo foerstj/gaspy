f8bfb92125e7bee16f91d9f85d39030243810fc0
Johannes Förstner <johannes@famfoerstner.de>
2023-01-01 10:55:35 +0100

    Fix printout centroid_offset, print in ksy order, omit redundant/useless info

--- sno/sno_handler.py
+++ sno/sno_handler.py
@@ -14,8 +14,9 @@ class SnoHandler:
         print(f'{indent}texture: {surface.texture}')
 
     @classmethod
-    def print_sno(cls, sno: Sno, indent=''):
-        print(f'{indent}magic: {sno.magic}')
+    def print_sno(cls, sno: Sno, indent='', redundant=False):
+        if redundant:
+            print(f'{indent}magic: {sno.magic}')
         print(f'{indent}version: {sno.version.major}.{sno.version.minor}')
         print(f'{indent}door count: {sno.door_count}')
         print(f'{indent}spot count: {sno.spot_count}')
@@ -23,14 +24,15 @@ class SnoHandler:
         print(f'{indent}triangle count: {sno.triangle_count}')
         print(f'{indent}texture count: {sno.texture_count}')
         print(f'{indent}bounding box: {cls.v3_str(sno.bounding_box.min)} - {cls.v3_str(sno.bounding_box.max)}')
-        print(f'{indent}centroid offset: {sno.centroid_offset}')
+        print(f'{indent}centroid offset: {cls.v3_str(sno.centroid_offset)}')
         print(f'{indent}tile: {sno.tile}')
-        print(f'{indent}reserved 0-2: {sno.reserved0} {sno.reserved1} {sno.reserved2}')
-        print(f'{indent}checksum: {sno.checksum}')
-        print(f'{indent}logical mesh count: {sno.logical_mesh_count}')
+        if redundant:
+            print(f'{indent}reserved 0-2: {sno.reserved0} {sno.reserved1} {sno.reserved2}')
+            print(f'{indent}checksum: {sno.checksum}')
         print(f'{indent}surfaces:')
         for surface in sno.surface_array:
             cls.print_surface(surface, indent + '  ')
+        print(f'{indent}logical mesh count: {sno.logical_mesh_count}')
 
-    def print(self, indent=''):
-        self.print_sno(self.sno, indent)
+    def print(self, indent='', redundant=False):
+        self.print_sno(self.sno, indent, redundant)
