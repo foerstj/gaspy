8d0f21662de48108a4002b6975d191ee61a81943
Johannes Förstner <johannes@famfoerstner.de>
2023-02-17 08:08:37 +0100

    Add own CLI for more options

--- printouts/spells.py
+++ printouts/spells.py
@@ -1,4 +1,8 @@
+import argparse
+import sys
+
 from bits.bits import Bits
+from gas.gas_parser import GasParser
 from printouts.csv import write_csv
 
 
@@ -18,4 +22,30 @@ def write_spells_csv(bits: Bits):
         assert magic_class in ['mc_nature_magic', 'mc_combat_magic']
         nc = 'N' if magic_class == 'mc_nature_magic' else 'C'
         csv.append([template_name, spell_name, 'SCROLL' if is_scroll else '', pm, nc])
+    print(f'CSV: {len(csv)-1} data rows')
     write_csv('Spells', csv)
+
+
+# Own CLI for more options
+
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy printouts spells')
+    #parser.add_argument('--only-for', choices=['player', 'monster'])
+    parser.add_argument('--bits', default=None)
+    return parser
+
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
+    GasParser.get_instance().print_warnings = False
+    bits = Bits(args.bits)
+    write_spells_csv(bits)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
