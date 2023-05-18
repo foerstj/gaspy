c4f8dccfe2eeb516c7aa6e9246ef515ff736f727
Johannes Förstner <johannes@famfoerstner.de>
2023-05-18 09:32:02 +0200

    Load all properties (WOWH)

--- bits/maps/quests_gas.py
+++ bits/maps/quests_gas.py
@@ -3,24 +3,33 @@ from gas.gas_file import GasFile
 
 
 class ChapterUpdate:
-    def __init__(self, description: str):
+    def __init__(self, description: str, order: int, sample: str):
         self.description = description
+        self.order = order
+        self.sample = sample
 
 
 class Chapter:
-    def __init__(self, screen_name: str, updates: list[ChapterUpdate]):
+    def __init__(self, screen_name: str, chapter_image: str, updates: list[ChapterUpdate]):
         self.screen_name = screen_name
+        self.chapter_image = chapter_image
         self.updates = updates
 
 
 class QuestUpdate:
-    def __init__(self, description: str):
+    def __init__(self, description: str, required: bool, order: int, speaker: str, address: str):
         self.description = description
+        self.required = required
+        self.order = order
+        self.speaker = speaker
+        self.address = address
 
 
 class Quest:
-    def __init__(self, screen_name: str, updates: list[QuestUpdate]):
+    def __init__(self, chapter: str, screen_name: str, quest_image: str, updates: list[QuestUpdate]):
+        self.chapter = chapter
         self.screen_name = screen_name
+        self.quest_image = quest_image
         self.updates = updates
 
 
@@ -37,9 +46,14 @@ class QuestsGas:
             name = chapter_section.header
             updates = list()
             for update_section in chapter_section.get_sections():
-                updates.append(ChapterUpdate(update_section.get_attr_value('description')))
-            quest = Chapter(chapter_section.get_attr_value('screen_name'), updates)
-            chapters[name] = quest
+                description = update_section.get_attr_value('description')
+                order = update_section.get_attr_value('order')
+                sample = update_section.get_attr_value('sample')
+                updates.append(ChapterUpdate(description, order, sample))
+            screen_name = chapter_section.get_attr_value('screen_name')
+            chapter_image = chapter_section.get_attr_value('chapter_image')
+            chapter = Chapter(screen_name, chapter_image, updates)
+            chapters[name] = chapter
         return chapters
 
     @classmethod
@@ -49,8 +63,16 @@ class QuestsGas:
             name = quest_section.header
             updates = list()
             for update_section in quest_section.get_sections():
-                updates.append(QuestUpdate(update_section.get_attr_value('description')))
-            quest = Quest(quest_section.get_attr_value('screen_name'), updates)
+                description = update_section.get_attr_value('description')
+                required = update_section.get_attr_value('required')
+                order = update_section.get_attr_value('order')
+                speaker = update_section.get_attr_value('speaker')
+                address = update_section.get_attr_value('address')
+                updates.append(QuestUpdate(description, required, order, speaker, address))
+            chapter = quest_section.get_attr_value('chapter')
+            screen_name = quest_section.get_attr_value('screen_name')
+            quest_image = quest_section.get_attr_value('quest_image')
+            quest = Quest(chapter, screen_name, quest_image, updates)
             quests[name] = quest
         return quests
 
