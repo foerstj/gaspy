f216832517c4fb0e40df086e32dc5558bf479d1d
Johannes Förstner <johannes@famfoerstner.de>
2023-05-18 08:41:11 +0200

    Split method, add TODO

--- region_import/rename_region.py
+++ region_import/rename_region.py
@@ -2,26 +2,20 @@ import os
 import sys
 
 from bits.bits import Bits
+from bits.maps.map import Map
 
 
-# Renames a region.
-# 1. The region folder is renamed (duh)
-# 2. References in stitch helper files are adapted
-def rename_region(map_name, old_region_name, new_region_name):
-    bits = Bits()
-    assert map_name in bits.maps, map_name
-    m = bits.maps[map_name]
-    regions = m.get_regions()
-    assert old_region_name in regions, old_region_name
-    assert new_region_name not in regions, new_region_name
-    region = regions[old_region_name]
+def rename_region_folder(m: Map, old_region_name: str, new_region_name: str):
+    region = m.get_region(old_region_name)
     src_path = region.gas_dir.path
     dst_path = os.path.join(os.path.dirname(src_path), new_region_name)
     print('src: '+src_path)
     print('dst: '+dst_path)
     os.rename(src_path, dst_path)
-
     m.gas_dir.clear_cache()
+
+
+def rename_region_refs_stitches(m: Map, old_region_name: str, new_region_name: str):
     regions = m.get_regions()
     for region_name, r in regions.items():
         stitch_helper_file = r.gas_dir.get_subdir('editor').get_gas_file('stitch_helper')
@@ -38,6 +32,23 @@ def rename_region(map_name, old_region_name, new_region_name):
                     section.set_t_n_header(t, new_region_name)
                     section.set_attr_value('dest_region', new_region_name)
         stitch_helper_file.save()
+
+
+# Renames a region.
+# 1. The region folder is renamed (duh)
+# 2. References in stitch helper files are adapted
+# TODO 3. references in quest convos
+def rename_region(map_name, old_region_name, new_region_name):
+    bits = Bits()
+    assert map_name in bits.maps, map_name
+    m = bits.maps[map_name]
+    regions = m.get_regions()
+    assert old_region_name in regions, old_region_name
+    assert new_region_name not in regions, new_region_name
+
+    rename_region_folder(m, old_region_name, new_region_name)
+
+    rename_region_refs_stitches(m, old_region_name, new_region_name)
 
 
 def main(argv):
