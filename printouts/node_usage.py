f1c04231b296970cd06ec684a3a40d9618aa35fa
Johannes Förstner <johannes@famfoerstner.de>
2023-07-27 17:02:52 +0200

    Print summary of usage value counts

--- printouts/node_usage.py
+++ printouts/node_usage.py
@@ -10,15 +10,21 @@ from gas.gas_parser import GasParser
 
 def get_node_usage(m: Map, usages: dict):
     for region in m.get_regions().values():
+        num_nodes = len(region.get_terrain().nodes)
+        num_meshes = len(region.get_node_meshes())
+        print(f'  {region.get_name()}: {num_nodes} nodes, {num_meshes} meshes')
         for node_mesh_name in region.get_node_meshes():
             assert node_mesh_name in usages, node_mesh_name
             usages[node_mesh_name] = True
 
 
-def node_usage(bits: Bits, node_bits: Bits):
+def node_usage(count_usage_values=False, bits_path=None, node_bits_path=None):
+    GasParser.get_instance().print_warnings = False
+    bits = Bits(bits_path)
+    node_bits = bits if node_bits_path is None else Bits(node_bits_path)
+
     snos = node_bits.snos
     print(f'SNOs: {len(snos.snos)}')
-    # snos.print('  ', None)
     usages = {SNOs.get_name_for_path(sno_path): None for sno_path in snos.snos}
 
     maps = bits.maps
@@ -30,10 +36,18 @@ def node_usage(bits: Bits, node_bits: Bits):
     print('Usages:')
     for node_mesh_name, usage in usages.items():
         print(f'  {node_mesh_name}: {usage}')
+    if count_usage_values:
+        usage_value_counts = {v: 0 for v in usages.values()}
+        for usage_value in usages.values():
+            usage_value_counts[usage_value] += 1
+        print('Usage value summary:')
+        for usage_value, count in usage_value_counts.items():
+            print(f'  {usage_value}: {count}')
 
 
 def init_arg_parser():
     parser = argparse.ArgumentParser(description='GasPy printouts node_usage')
+    parser.add_argument('--count-usage-values', action='store_true')
     parser.add_argument('--bits', default=None)
     parser.add_argument('--node-bits', default=None)
     return parser
@@ -46,10 +60,7 @@ def parse_args(argv):
 
 def main(argv):
     args = parse_args(argv)
-    GasParser.get_instance().print_warnings = False
-    bits = Bits(args.bits)
-    node_bits = bits if args.node_bits is None else Bits(args.node_bits)
-    node_usage(bits, node_bits)
+    node_usage(args.count_usage_values, args.bits, args.node_bits)
 
 
 if __name__ == '__main__':
