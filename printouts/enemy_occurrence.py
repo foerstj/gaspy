c215ef3200d96e1fc3f20e2feb622f2bc368580e
Johannes Förstner <johannes@famfoerstner.de>
2023-08-29 16:50:39 +0200

    Take generated enemies into account for stats enemy-occurrence

--- printouts/enemy_occurrence.py
+++ printouts/enemy_occurrence.py
@@ -15,7 +15,9 @@ def print_enemy_occurrence(bits: Bits):
         for rxp in region_xp:
             region = rxp.region
             region_enemies = region.get_enemy_actors()
+            region_gen_enemies = region.get_generated_enemies()
             region_enemy_template_names = {e.template_name for e in region_enemies}
+            region_enemy_template_names.update(region_gen_enemies.keys())
             region_enemy_strs = [tn + ' ('+str(enemies_by_tn[tn].xp)+' XP)' for tn in region_enemy_template_names]
             region_enemies_str = str(len(region_enemy_template_names)) + ' enemy types: ' + ', '.join(region_enemy_strs)
             print('Region ' + region.get_name() + ' (XP '+str(rxp.xp_pre)+' - '+str(rxp.xp_post)+') contains ' + region_enemies_str)
