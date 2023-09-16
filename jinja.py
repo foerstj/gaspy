9a5e559142965c5e171d94baffbfe1c1ef1610ae
Johannes Förstner <johannes@famfoerstner.de>
2023-09-16 11:02:28 +0200

    Parse cell data; open template & render with data

--- jinja.py
+++ jinja.py
@@ -14,20 +14,34 @@ def parse_args(argv):
     return parser.parse_args(argv)
 
 
+def parse_cell(cell: str):
+    try:
+        return int(cell)
+    except:
+        pass
+
+    try:
+        return float(cell)
+    except:
+        pass
+
+    return cell
+
+
 def load_csv_as_dicts(csv_file_path):
     data_dicts = list()
     with open(csv_file_path, 'r') as csv_file:
         lines = csv_file.readlines()
         header_line = lines[0]
-        header_cells = header_line.split(';')
+        header_cells = [c.strip() for c in header_line.split(';')]
         data_lines = lines[1:]
         for data_line in data_lines:
-            data_cells = data_line.split(';')
+            data_cells = [parse_cell(c.strip()) for c in data_line.split(';')]
             data_dict = dict()
             for i in range(0, len(header_cells)):
                 header_cell = header_cells[i]
                 data_cell = data_cells[i]
-                data_dict[header_cell.strip()] = data_cell.strip()
+                data_dict[header_cell] = data_cell
             data_dicts.append(data_dict)
     return data_dicts
 
@@ -38,12 +52,18 @@ def jinja(bits_dir):
         loader=FileSystemLoader(bits.gas_dir.path),
         autoescape=select_autoescape()
     )
-    jinja_src_path = path.join(bits.gas_dir.path, 'world', 'contentdb', 'templates.jinja')
-    if path.exists(jinja_src_path):
-        for jinja_file_path in Path(jinja_src_path).rglob('*.jinja'):
-            csv_file_path = str(jinja_file_path)[:-6] + '.csv'
-            data_dicts = load_csv_as_dicts(path.join(str(jinja_src_path), csv_file_path))
+    rel_jinja_src_path = path.join('world', 'contentdb', 'templates.jinja')
+    abs_jinja_src_path = path.join(bits.gas_dir.path, rel_jinja_src_path)
+    if path.exists(abs_jinja_src_path):
+        for globbed_file_path in Path(abs_jinja_src_path).rglob('*.jinja'):
+            jinja_file_path = str(path.relpath(globbed_file_path, abs_jinja_src_path))
+            base_file_path = jinja_file_path[:-6]
+            csv_file_path = base_file_path + '.csv'
+            data_dicts = load_csv_as_dicts(path.join(abs_jinja_src_path, csv_file_path))
             print(data_dicts)
+            template = env.get_template(path.join(rel_jinja_src_path, jinja_file_path).replace('\\', '/'))
+            for data_dict in data_dicts:
+                print(template.render(**data_dict))
 
 
 def main(argv):
