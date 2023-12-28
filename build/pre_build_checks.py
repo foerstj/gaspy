228c4f1051afbe95d57074c7dfb08071d22e3c9e
Johannes Förstner <johannes@famfoerstner.de>
2023-12-28 19:41:36 +0100

    Start new script: one pre_build_checks to rule them all

---
+++ build/pre_build_checks.py
@@ -0,0 +1,36 @@
+import argparse
+import sys
+
+from bits.bits import Bits
+from build.check_tips import check_tips
+
+
+def pre_build_checks(bits_path: str, map_name: str, checks: list[str]) -> bool:
+    bits = Bits(bits_path)
+    valid = True
+    if 'tips' in checks:
+        valid &= check_tips(bits, map_name)
+    return valid
+
+
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy pre_build_checks')
+    parser.add_argument('map')
+    parser.add_argument('--check', nargs='+', choices=['tips'])
+    parser.add_argument('--bits', default='DSLOA')
+    return parser
+
+
+def parse_args(argv: list[str]):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
+def main(argv: list[str]) -> int:
+    args = parse_args(argv)
+    valid = pre_build_checks(args.bits, args.map, args.check)
+    return 0 if valid else -1
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv[1:]))
