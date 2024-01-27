31818241a0a2d14d120f7c24f3d7255e15e42387
Johannes Förstner <johannes@famfoerstner.de>
2024-01-27 17:35:17 +0100

    Load more attrs of convo items: order, choice, scroll_rate

--- bits/maps/conversations_gas.py
+++ bits/maps/conversations_gas.py
@@ -11,8 +11,13 @@ class ConversationItemButton:
 
 
 class ConversationItem:
-    def __init__(self, screen_text: str = None, activate_quests: list[str] = None, complete_quests: list[str] = None, buttons: dict[int, ConversationItemButton] = None):
+    def __init__(self, screen_text: str = None, order: int = None, choice: str = None, scroll_rate: float = None, activate_quests: list[str] = None, complete_quests: list[str] = None, buttons: dict[int, ConversationItemButton] = None):
+        if choice is not None:
+            assert choice in ['more', 'shop', 'potential_member', 'buy_packmule']
         self.screen_text = screen_text
+        self.order = order
+        self.choice = choice
+        self.scroll_rate = scroll_rate
         self.activate_quests = activate_quests or list()  # list of "quest_name(,i)"
         self.complete_quests = complete_quests or list()  # list of quest names
         self.buttons = buttons
@@ -23,6 +28,10 @@ class ConversationItem:
 
         screen_text = item_section.get_attr_value('screen_text')
 
+        order = item_section.get_attr_value('order')
+        choice = item_section.get_attr_value('choice')
+        scroll_rate = item_section.get_attr_value('scroll_rate')
+
         activate_quests = [a.value for a in item_section.get_attrs('activate_quest*')]
         complete_quests = [a.value for a in item_section.get_attrs('complete_quest*')]
 
@@ -31,7 +40,7 @@ class ConversationItem:
             button_1 = ConversationItemButton(item_section.get_attr_value('button_1_text'), item_section.get_attr_value('button_1_value'))
             buttons = {1: button_1}
 
-        return ConversationItem(screen_text, activate_quests, complete_quests, buttons)
+        return ConversationItem(screen_text, order, choice, scroll_rate, activate_quests, complete_quests, buttons)
 
     # Not fit for use - fields incomplete!
     def to_gas(self) -> Section:
