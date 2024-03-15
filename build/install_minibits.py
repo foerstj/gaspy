1c9e1725152113bd4f0f2540451e5af6b5423504
Johannes Förstner <johannes@famfoerstner.de>
2024-03-15 16:04:46 +0100

    Copy templates

--- build/install_minibits.py
+++ build/install_minibits.py
@@ -1,8 +1,32 @@
 import argparse
 import os
+import shutil
 import sys
+import time
 
 from bits.bits import Bits
+
+
+def install_minibits_templates(bits: Bits, minibits_single_path: str):
+    minibits_name = os.path.basename(os.path.dirname(minibits_single_path))
+    src_templates_path = os.path.join(minibits_single_path, 'world', 'contentdb', 'templates')
+    if not os.path.exists(src_templates_path):
+        return
+    dst_templates_path = os.path.join(bits.gas_dir.path, 'world', 'contentdb', 'templates', 'minibits', minibits_name)
+    shutil.copytree(src_templates_path, dst_templates_path)
+    time.sleep(0.1)  # shutil...
+
+
+def install_minibits_single(bits: Bits, minibits_path: str, minibits_single: str):
+    print(minibits_single)
+    minibits_single_path = os.path.join(minibits_path, minibits_single, 'Bits')
+    assert os.path.exists(minibits_single_path)
+    install_minibits_templates(bits, minibits_single_path)
+
+
+def install_minibits_list(bits: Bits, minibits_path: str, minibits_list: list[str]):
+    for minibits_single in minibits_list:
+        install_minibits_single(bits, minibits_path, minibits_single)
 
 
 def install_minibits(bits_path: str, minibits_path: str):
@@ -11,6 +35,10 @@ def install_minibits(bits_path: str, minibits_path: str):
     bits = Bits(bits_path)
     minibits_txt_path = os.path.join(bits.gas_dir.path, 'minibits.txt')
     assert os.path.exists(minibits_txt_path)
+    with open(minibits_txt_path, 'r') as minibits_txt:
+        minibits_list = minibits_txt.readlines()
+    minibits_list = [x.strip() for x in minibits_list]
+    install_minibits_list(bits, minibits_path, minibits_list)
     print('That\'s it folks!')
 
 
