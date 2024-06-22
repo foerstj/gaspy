4318e24e6ad0f0a09de091a30282682667f3b78f
Johannes Förstner <johannes@famfoerstner.de>
2024-06-22 22:08:09 +0200

    Print out what I'm interested in right now

--- printouts/map_levels.py
+++ printouts/map_levels.py
@@ -8,5 +8,6 @@ def write_map_levels_csv(m: Map):
     regions_xp = load_regions_xp(m)
     data = [['world level', 'region', 'xp', 'weight', 'xp', 'sum', 'level pre', 'level post']]
     for r in regions_xp:
+        print(f'{r.name} {r.xp*r.weight}')
         data.append([r.world_level, r.name, r.xp, r.weight, r.xp*r.weight, r.xp_post, r.pre_level, r.post_level])
     write_csv(m.gas_dir.dir_name, data)
