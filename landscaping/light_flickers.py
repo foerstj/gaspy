0a70089fa3e0a7eeaac874cf72d79c541ac2cc18
Johannes Förstner <johannes@famfoerstner.de>
2024-06-23 14:34:09 +0200

    Parameterize light_flickers somewhat

--- landscaping/light_flickers.py
+++ landscaping/light_flickers.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 from bits.bits import Bits
@@ -13,8 +14,7 @@ class LightFlickerTask:
         self.color = color
 
 
-def light_flickers(map_name: str, region_name: str, tasks: list[LightFlickerTask]):
-    bits = Bits()
+def do_light_flickers(bits: Bits, map_name: str, region_name: str, tasks: list[LightFlickerTask]):
     m = bits.maps[map_name]
     region = m.get_region(region_name)
     region.objects.load_objects()
@@ -33,7 +33,7 @@ def light_flickers(map_name: str, region_name: str, tasks: list[LightFlickerTask
             light = PointLight(Hex.random(), position=PosDir(pos.x, pos.y + 2, pos.z, pos.node_guid))
             light.color = task.color
             light.inner_radius = 1
-            light.outer_radius = 3
+            light.outer_radius = 4
             light.occlude_geometry = True
             light.draw_shadow = True
             region.get_lights().append(light)
@@ -44,14 +44,34 @@ def light_flickers(map_name: str, region_name: str, tasks: list[LightFlickerTask
         region.delete_lnc()
 
 
+def light_flickers(bits_dir: str, map_name: str, region_name: str, task_args: list[str]):
+    bits = Bits(bits_dir)
+    tasks = list()
+    for task_arg in task_args:
+        templates, color = task_arg.split('=')
+        templates = templates.split(',')
+        color = Color(Hex.parse(color))
+        tasks.append(LightFlickerTask(templates, color))
+    do_light_flickers(bits, map_name, region_name, tasks)
+
+
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy light_flickers')
+    parser.add_argument('map')
+    parser.add_argument('region')
+    parser.add_argument('--add', nargs='+', help='--add crystals_cav_05,crystals_cav_09=0xff20ffff crystals_cav_06,crystals_cav_10=0xffff20ff')
+    parser.add_argument('--bits', default=None)
+    return parser
+
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
 def main(argv):
-    tasks = [
-        LightFlickerTask(['crystals_cav_01_x2', 'crystals_cav_05_x2', 'crystals_cav_09_x2', 'crystals_cav_13_x2'], Color.from_argb(255, 32, 255, 255)),  # teal
-        LightFlickerTask(['crystals_cav_02_x2', 'crystals_cav_06_x2', 'crystals_cav_10_x2', 'crystals_cav_14_x2'], Color.from_argb(255, 255, 32, 255)),  # purple
-        LightFlickerTask(['crystals_cav_03_x2', 'crystals_cav_07_x2', 'crystals_cav_11_x2', 'crystals_cav_15_x2'], Color.from_argb(255, 32, 255, 32)),  # green
-        LightFlickerTask(['crystals_cav_04_x2', 'crystals_cav_08_x2', 'crystals_cav_12_x2', 'crystals_cav_16_x2'], Color.from_argb(255, 32, 32, 255)),  # blue
-    ]
-    light_flickers('collab24', '3_2_crystlayer', tasks)
+    args = parse_args(argv)
+    light_flickers(args.bits, args.map, args.region, args.add)
 
 
 if __name__ == '__main__':
