3cfde94f9562470e7fd0640d19b8a66f419ed00a
Johannes Förstner <johannes@famfoerstner.de>
2024-06-27 07:48:32 +0200

    Add radius params

--- landscaping/light_flickers.py
+++ landscaping/light_flickers.py
@@ -9,9 +9,10 @@ from gas.molecules import Position, Hex
 
 
 class LightFlickerTask:
-    def __init__(self, template_names: list[str], color: Color):
+    def __init__(self, template_names: list[str], color: Color, radius: (int, int)):
         self.template_names = template_names
         self.color = color
+        self.radius = radius
 
 
 def do_light_flickers(bits: Bits, map_name: str, region_name: str, tasks: list[LightFlickerTask], override: bool):
@@ -36,13 +37,14 @@ def do_light_flickers(bits: Bits, map_name: str, region_name: str, tasks: list[L
             pos: Position = oni.get_own_value('placement', 'position')
             light = PointLight(Hex.random(), position=PosDir(pos.x, pos.y + 2, pos.z, pos.node_guid))
             light.color = task.color
-            light.inner_radius = 1
-            light.outer_radius = 4
+            light.inner_radius = task.radius[0]
+            light.outer_radius = task.radius[1]
             light.occlude_geometry = True
             light.draw_shadow = True
             region.get_lights().append(light)
             num_lights_added += 1
             oni.section.get_or_create_section('light_flicker_lightweight').set_attr_value('siege_light', light.id)
+    print(f'Flicker lights added: {num_lights_added}')
     if num_lights_added:
         region.save()
         region.delete_lnc()
@@ -52,10 +54,12 @@ def light_flickers(bits_dir: str, map_name: str, region_name: str, task_args: li
     bits = Bits(bits_dir)
     tasks = list()
     for task_arg in task_args:
-        templates, color = task_arg.split('=')
+        templates, light_def = task_arg.split('=')
         templates = templates.split(',')
-        color = Color(Hex.parse(color))
-        tasks.append(LightFlickerTask(templates, color))
+        light_def_parts = light_def.split(':')
+        color = Color(Hex.parse(light_def_parts[0]))
+        i_rad, o_rad = [float(x) for x in light_def_parts[1].split('-')] if len(light_def_parts) > 1 else (1, 4)
+        tasks.append(LightFlickerTask(templates, color, (i_rad, o_rad)))
     do_light_flickers(bits, map_name, region_name, tasks, override)
 
 
