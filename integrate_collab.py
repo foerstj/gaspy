bc5966ad9b500963e7351fe6993e71c357c545f4
Johannes Förstner <johannes@famfoerstner.de>
2024-07-25 17:56:03 +0200

    Make sure to save changed regions

--- integrate_collab.py
+++ integrate_collab.py
@@ -85,13 +85,20 @@ def integrate_collab(path: str, name: str):
     # mend stitches
     print('mend stitches')
     for region_name, region in m.get_regions().items():
+        change_in_region = False
         for stitch_group in region.get_stitch_helper().stitch_editors:
             if stitch_group.dest_region not in m.get_regions():
                 first_stitch_id = list(stitch_group.node_ids.keys())[0]
                 for region_name_2, region_2 in m.get_regions().items():
+                    if region_name_2 == region_name:
+                        continue
                     for stitch_group_2 in region_2.get_stitch_helper().stitch_editors:
                         if first_stitch_id in stitch_group_2.node_ids:
                             stitch_group.dest_region = region_name_2
+                            # print(f'  {region_name} {first_stitch_id} -> {region_name_2}')
+                            change_in_region = True
+        if change_in_region:
+            region.save()
 
     print('integrate_collab done')
     m.save()
