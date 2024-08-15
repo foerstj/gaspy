23b1db6a9fe22936e6f38a0dd62d42f7b46c7214
Johannes Förstner <johannes@famfoerstner.de>
2024-08-15 09:02:35 +0200

    Code spat out by ChatGPT Python

---
+++ arithmetics.py
@@ -0,0 +1,53 @@
+import operator
+import re
+
+# Define supported operators
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+
+def eval_expression(expression, variables):
+    # Replace variables with their values in the expression
+    for var, value in variables.items():
+        expression = expression.replace(var, str(value))
+
+    # Tokenize and evaluate the expression
+    def parse_expression(tokens):
+        def parse_atom():
+            token = tokens.pop(0)
+            if token == '(':
+                result = parse_expression(tokens)
+                tokens.pop(0)  # remove ')'
+                return result
+            else:
+                return float(token)
+
+        def parse_term():
+            result = parse_atom()
+            while tokens and tokens[0] in ('*', '/'):
+                op = tokens.pop(0)
+                result = operators[op](result, parse_atom())
+            return result
+
+        result = parse_term()
+        while tokens and tokens[0] in ('+', '-'):
+            op = tokens.pop(0)
+            result = operators[op](result, parse_term())
+        return result
+
+    # Split the expression into tokens
+    tokens = re.findall(r'\d+|\+|\-|\*|\/|\(|\)', expression)
+    return parse_expression(tokens)
+
+
+# Define the expression and variable values
+expression = "(foo * 3) + 1"
+variables = {"foo": 5}
+
+# Evaluate the expression
+result = eval_expression(expression, variables)
+print(result)
