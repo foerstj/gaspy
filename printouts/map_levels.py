e5f6ead85d53ed90b3fa482b5428619b5e377b45
Johannes Förstner <johannes@famfoerstner.de>
2024-08-25 11:13:46 +0200

    Add own cli for map_levels.py

--- printouts/map_levels.py
+++ printouts/map_levels.py
@@ -1,5 +1,10 @@
 # Ordered regions -> how much XP, and what lvl the player will be at
+import argparse
+import sys
+
+from bits.bits import Bits
 from bits.maps.map import Map
+from gas.gas_parser import GasParser
 from printouts.common import load_regions_xp
 from printouts.csv import write_csv
 
@@ -11,3 +16,30 @@ def write_map_levels_csv(m: Map):
         print(f'{r.name} {r.xp*r.weight}')
         data.append([r.world_level, r.name, r.xp, r.weight, r.xp*r.weight, r.xp_post, r.pre_level, r.post_level])
     write_csv(m.gas_dir.dir_name, data)
+
+
+def map_levels(map_name: str, bits_path: str):
+    bits = Bits(bits_path)
+    m = bits.maps[map_name]
+    write_map_levels_csv(m)
+
+
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy map_levels')
+    parser.add_argument('map')
+    parser.add_argument('--bits', default=None)
+    return parser
+
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
+    map_levels(args.map, args.bits)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
