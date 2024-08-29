03900efb22c741f0bb6638206930c9377a702e98
Johannes Förstner <johannes@famfoerstner.de>
2024-08-29 18:41:14 +0200

    Move sub-function out

--- arithmetics.py
+++ arithmetics.py
@@ -10,38 +10,40 @@ operators = {
 }
 
 
+# Tokenize and evaluate the expression
+def parse_expression(tokens: list[str]):
+    def parse_atom():
+        token = tokens.pop(0)
+        if token == '(':
+            result = parse_expression(tokens)
+            tokens.pop(0)  # remove ')'
+            return result
+        else:
+            return float(token)
+
+    def parse_term():
+        result = parse_atom()
+        while tokens and tokens[0] in ('*', '/'):
+            op = tokens.pop(0)
+            result = operators[op](result, parse_atom())
+        return result
+
+    result = parse_term()
+    while tokens and tokens[0] in ('+', '-'):
+        op = tokens.pop(0)
+        result = operators[op](result, parse_term())
+    return result
+
+
 def eval_expression(expression: str, variables: dict = None):
     # Replace variables with their values in the expression
     if variables is not None:
         for var, value in variables.items():
             expression = expression.replace(var, str(value))
 
-    # Tokenize and evaluate the expression
-    def parse_expression(tokens):
-        def parse_atom():
-            token = tokens.pop(0)
-            if token == '(':
-                result = parse_expression(tokens)
-                tokens.pop(0)  # remove ')'
-                return result
-            else:
-                return float(token)
-
-        def parse_term():
-            result = parse_atom()
-            while tokens and tokens[0] in ('*', '/'):
-                op = tokens.pop(0)
-                result = operators[op](result, parse_atom())
-            return result
-
-        result = parse_term()
-        while tokens and tokens[0] in ('+', '-'):
-            op = tokens.pop(0)
-            result = operators[op](result, parse_term())
-        return result
-
     # Split the expression into tokens
     tokens = re.findall(r'[\d.]+|\+|\-|\*|\/|\(|\)', expression)
+
     return parse_expression(tokens)
 
 
