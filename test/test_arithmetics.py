096d1a311625f076dab1305d4494376e7a695f9b
Johannes Förstner <johannes@famfoerstner.de>
2024-08-29 18:36:01 +0200

    Add more test cases

--- test/test_arithmetics.py
+++ test/test_arithmetics.py
@@ -23,5 +23,17 @@ class TestArithmetics(unittest.TestCase):
         self.assertEqual(eval_expression('1+(2*(3-(10/5)))'), 3)
         self.assertEqual(eval_expression('(2+3)*(2-3)'), -5)
 
-    def test_variables(self):
+    def test_variables_general(self):
         self.assertEqual(eval_expression('a', {'a': 7}), 7)
+        self.assertEqual(eval_expression('(a+1)*2', {'a': 7}), 16)
+        self.assertAlmostEqual(eval_expression('a+b-c*d/e', {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}), 0.6)
+
+    def test_variables_ds1(self):
+        # spell_summon_braak_desert
+        self.assertEqual(eval_expression('(#magic+1)*16-.7', {'#magic': 69}), 1119.3)  # mana_cost_modifier
+        self.assertAlmostEqual(eval_expression('((((#magic*0.64)+10)*.56)-#str)', {'#magic': 69, '#str': 19}), 11.3296)  # alter_strength
+        self.assertEqual(eval_expression('((((#magic*4.2)*.9)-194)*0.9)', {'#magic': 69}), 60.138)  # alter_cmagic_damage_min
+
+    def test_variables_ds2(self):
+        # ds2 style merc/vet/elite formula for monster_level
+        self.assertEqual(eval_expression('(12.0 * #is_normal) + (51.0 * #is_veteran) + (78.0 * #is_elite)', {'#is_normal': 0, '#is_veteran': 1, '#is_elite': 0}), 51)  # boggrot_stats
