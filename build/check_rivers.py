9b9af6bc7ec55ce8d28372b5b786cda8b02b14d3
Johannes Förstner <johannes@famfoerstner.de>
2025-09-13 13:17:31 +0200

    Add data for multiplayer_world

--- build/check_rivers.py
+++ build/check_rivers.py
@@ -14,6 +14,9 @@ RIVER_DATA = {
     't_fh00_wfall_1b': {5: IN, 1: OUT},
     't_fh00_wheeletc': {6: IN, 7: IN, 1: OUT, 2: OUT},
 
+    't_xxx_rmp_tx-rvr-in': {1: IN, 2: IN},
+    't_xxx_rmp_tx-rvr-out': {1: OUT, 2: OUT},
+
     't_xxx_rvr_08-bridge-left-tx-right-a': {4: IN, 1: OUT},
     't_xxx_rvr_08-bridge-right-tx-left-a': {4: IN, 1: OUT},
     't_xxx_rvr_08-left-a': {4: IN, 1: OUT},
@@ -24,29 +27,55 @@ RIVER_DATA = {
     't_xxx_rvr_08-right-b': {4: IN, 1: OUT},
     't_xxx_rvr_08-right-turn': {5: IN, 6: OUT},
     't_xxx_rvr_08-right-tx-left-a': {4: IN, 1: OUT},
+    't_xxx_rvr_12-bridge-a': {5: IN, 6: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_12-left-a': {3: IN, 1: OUT},
     't_xxx_rvr_12-left-b': {3: IN, 1: OUT},
     't_xxx_rvr_12-left-c': {3: IN, 1: OUT},
     't_xxx_rvr_12-right-a': {4: IN, 1: OUT},
     't_xxx_rvr_12-right-b': {4: IN, 1: OUT},
     't_xxx_rvr_12-right-c': {4: IN, 1: OUT},
+    't_xxx_rvr_12-stonebridge-cobble-a': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-cobble-b': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-cobble-c': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-granite-a': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-granite-b': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-granite-c': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-marble-a': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-marble-b': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-marble-c': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-ruins-a': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-ruins-b': {5: IN, 6: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_12-stonebridge-ruins-c': {5: IN, 6: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_12-tx-08-left-a': {5: IN, 6: IN, 1: OUT},
     't_xxx_rvr_12-tx-08-left-b': {6: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_12-tx-08-right-a': {5: IN, 6: IN, 2: OUT},
     't_xxx_rvr_12-tx-08-right-b': {5: IN, 1: OUT, 2: OUT},
-    't_xxx_rvr_wfall-alcove-base-a': {14: IN, 15: IN, 3: OUT, 4: OUT},
+    't_xxx_rvr_wfall-02a-tx-08-left': {3: IN, 1: OUT},
+    't_xxx_rvr_wfall-02a-tx-08-right': {3: IN, 1: OUT},
+    't_xxx_rvr_wfall-02a-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-04-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-04-tx-split-08': {7: IN, 8: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-04-tx-wal-02': {6: IN, 7: IN},
     't_xxx_rvr_wfall-08-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-08-tx-split-08': {7: IN, 8: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-08-tx-wal-02': {6: IN, 7: IN},
     't_xxx_rvr_wfall-12-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-12-tx-split-08': {7: IN, 8: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-12-tx-wal-02': {6: IN, 7: IN},
     't_xxx_rvr_wfall-24-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-24-tx-split-08': {7: IN, 8: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-24-tx-wal-02': {6: IN, 7: IN},
     't_xxx_rvr_wfall-32-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-32-tx-split-08': {7: IN, 8: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-32-tx-wal-02': {6: IN, 7: IN},
+    't_xxx_rvr_wfall-alcove-base-a': {14: IN, 15: IN, 3: OUT, 4: OUT},
     't_xxx_rvr_wfall-rmp-tx-12': {4: IN, 5: IN, 1: OUT, 2: OUT},
     't_xxx_rvr_wfall-rmp-tx-wal-02': {6: IN, 7: IN},
+    't_xxx_rvr_wfall-water-04-tx-wal-12': {1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-water-08-tx-wal-12': {1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-water-12-tx-wal-12': {1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-water-24-tx-wal-12': {1: OUT, 2: OUT},
+    't_xxx_rvr_wfall-water-36-tx-wal-12': {1: OUT, 2: OUT},
 
     't_xxx_wal_04-tx-rvr-in': {1: IN, 2: IN},
     't_xxx_wal_04-tx-rvr-out': {1: OUT, 2: OUT},
@@ -60,6 +89,9 @@ RIVER_DATA = {
     't_xxx_wal_32-tx-rvr-out': {1: OUT, 2: OUT},
 
     't_grs01_waterfall-alcove-d': {9: OUT, 10: OUT},
+
+    't_grs02_rvr-left-a-tx-grs01-a': {4: IN, 1: OUT},
+    't_grs02_rvr-right-b-tx-dc01-a': {4: IN, 1: OUT},
 }
 
 
@@ -77,6 +109,7 @@ def check_rivers_in_region(region: Region) -> int:
                     continue  # door not connected
                 other_node, other_door = node.doors[door]
                 assert isinstance(other_node, TerrainNode)
+                assert other_node.mesh_name in RIVER_DATA, f'Missing river data for {other_node.mesh_name} in region {region.get_name()}'
                 other_node_river_data = RIVER_DATA[other_node.mesh_name]
                 if node_river_data[door] == other_node_river_data[other_door]:
                     flow = 'IN' if node_river_data[door] else 'OUT'
