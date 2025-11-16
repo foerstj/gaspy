9e2041d4fd91bfe519c65580c634955a73c00b62
Johannes Förstner <johannes@famfoerstner.de>
2025-11-16 17:15:48 +0100

    Minimize logic inside "with open"

--- printouts/csv.py
+++ printouts/csv.py
@@ -11,8 +11,9 @@ def csv_cell(data) -> str:
 
 def write_csv(name: str, data: list[list], sep=','):
     out_file_path = os.path.join('output', f'{name}.csv')
+    lines = [sep.join([csv_cell(x) for x in y]) + '\n' for y in data]
     with open(out_file_path, 'w') as csv_file:
-        csv_file.writelines([sep.join([csv_cell(x) for x in y]) + '\n' for y in data])
+        csv_file.writelines(lines)
     print(f'wrote {out_file_path}')
 
 
