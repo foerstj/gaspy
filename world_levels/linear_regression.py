ab22e0d1f56cf33f7b4a366aeac98af7f127b2d4
Johannes Förstner <johannes@famfoerstner.de>
2025-11-16 13:00:31 +0100

    Printout that works for both

--- world_levels/linear_regression.py
+++ world_levels/linear_regression.py
@@ -11,14 +11,12 @@ from world_levels.world_level_templates import STAT_ATTRS
 
 REG_VARS_SELF = {stat: [stat] for stat in STAT_ATTRS}
 
-REG_VARS_XP = {stat: [stat, 'experience_value' if stat is not 'experience_value' else 'max_life'] for stat in STAT_ATTRS}
+REG_VARS_XP = {stat: [stat, 'experience_value' if stat != 'experience_value' else 'max_life'] for stat in STAT_ATTRS}
 
 
-def calc_linear_regression(bits: Bits, wl: str):
+def calc_linear_regression(bits: Bits, wl: str, regression_vars: dict):
     actors = bits.templates.get_actor_templates()
     wls_actors = get_wl_templates(actors)
-
-    reg_vars = REG_VARS_XP
 
     stats_vals = {s: list() for s in STAT_ATTRS}
     for name, wl_actors in wls_actors.items():
@@ -30,14 +28,14 @@ def calc_linear_regression(bits: Bits, wl: str):
         regular_stats = wl_actor_dict(regular_actor)
         wl_stats = wl_actor_dict(wl_actor)
 
-        for stat, stat_reg_vars in reg_vars.items():
+        for stat, stat_reg_vars in regression_vars.items():
             regular_value = regular_stats[stat]
             if regular_value is None:
                 continue
             if not regular_value:  # skip zeroes - zero values remain zero values in V/E
                 continue
             regular_values = [regular_stats[var] for var in stat_reg_vars]
-            if None in regular_values:
+            if None in regular_values or 0 in regular_values or 0.0 in regular_values:
                 continue
             wl_value = wl_stats[stat]
             stats_vals[stat].append((regular_values, wl_value))
@@ -48,8 +46,8 @@ def calc_linear_regression(bits: Bits, wl: str):
         y = [v[1] for v in stats_vals[stat]]
         x = [[v[i] for v in x] for i in range(len(x[0]))]  # flip around
         x = numpy.vstack([x, numpy.ones(len(x[0]))]).T
-        a, b, c = numpy.linalg.lstsq(x, y, rcond=None)[0]
-        lins[stat] = (a, b, c)
+        coeffs = tuple(numpy.linalg.lstsq(x, y, rcond=None)[0])
+        lins[stat] = coeffs
 
     return lins
 
@@ -57,11 +55,16 @@ def calc_linear_regression(bits: Bits, wl: str):
 def linear_regression(bits_path: str, wl: str):
     bits = Bits(bits_path)
 
-    lins = calc_linear_regression(bits, wl)
+    reg_vars = REG_VARS_XP
+    lins = calc_linear_regression(bits, wl, reg_vars)
 
     print()
-    for stat, (a, b, c) in lins.items():
-        print(f'{wl} {stat} = {a:.3f} * regular {stat} + {b:.3f} * regular xp/life + {c:.3f}')
+    for stat, coeffs in lins.items():
+        stat_reg_vars = reg_vars[stat]
+        assert len(coeffs) == len(stat_reg_vars) + 1
+        coeff_strs = [f'{coeffs[i]:.3f} * regular {var}' for i, var in enumerate(stat_reg_vars)] + [f'{coeffs[-1]:.3f}']
+        coeffs_str = ' + '.join(coeff_strs)
+        print(f'{wl} {stat} = {coeffs_str}')
 
 
 def init_arg_parser():
