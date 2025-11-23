a589cd21c5c746ffadbc2a3e2cb650a185d25230
Johannes Förstner <johannes@famfoerstner.de>
2025-11-23 14:50:27 +0100

    Filter npcs with convos and print their convo names

--- printouts/npcs.py
+++ printouts/npcs.py
@@ -60,7 +60,7 @@ def get_race(base_model):
     return race
 
 
-def printout_npc(npc: GameObject):
+def printout_npc(npc: GameObject, with_convos=False):
     template_name = npc.template_name
     screen_name = npc.compute_value('common', 'screen_name').strip('"')
     model = npc.compute_value('aspect', 'model')
@@ -68,32 +68,44 @@ def printout_npc(npc: GameObject):
     base_model, sub_model, base_model_pretty = parse_model_name(model)
     gender = get_gender(base_model, texture)
     race = get_race(base_model)
-    print(f'{template_name} "{screen_name}" - {model} {texture} - {base_model_pretty} - {race} {gender}')
+    convos_str = ''
+    if with_convos:
+        convo_attrs = npc.section.get_section('conversation').get_section('conversations').get_attrs()
+        convo_names = [a.value for a in convo_attrs]
+        convos_str = ' - ' + ', '.join(convo_names)
+    print(f'{template_name} "{screen_name}" - {model} {texture} - {base_model_pretty} - {race} {gender}' + convos_str)
 
 
-def printout_region_npcs(region: Region):
+def has_convos(npc: GameObject):
+    return npc.section.get_section('conversation') is not None
+
+
+def printout_region_npcs(region: Region, with_convos=False):
     npcs = region.get_npcs()
+    if with_convos:
+        npcs = [npc for npc in npcs if has_convos(npc)]
     if len(npcs) > 0:
         print(f'{region.get_name()}:')
     for npc in npcs:
-        printout_npc(npc)
+        printout_npc(npc, with_convos)
 
 
-def printout_map_npcs(m: Map):
+def printout_map_npcs(m: Map, with_convos=False):
     for region in m.get_regions().values():
-        printout_region_npcs(region)
+        printout_region_npcs(region, with_convos)
 
 
-def printout_npcs(map_name: str, bits_path: str):
+def printout_npcs(map_name: str, bits_path: str, with_convos=False):
     bits = Bits(bits_path)
     m = bits.maps[map_name]
     print(map_name)
-    printout_map_npcs(m)
+    printout_map_npcs(m, with_convos)
 
 
 def init_arg_parser():
     parser = argparse.ArgumentParser(description='GasPy printout npcs')
     parser.add_argument('map')
+    parser.add_argument('--with-convos', action='store_true')
     parser.add_argument('--bits', default=None)
     return parser
 
@@ -105,7 +117,7 @@ def parse_args(argv):
 
 def main(argv):
     args = parse_args(argv)
-    printout_npcs(args.map, args.bits)
+    printout_npcs(args.map, args.bits, args.with_convos)
 
 
 if __name__ == '__main__':
