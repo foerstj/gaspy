4041043f7c00de0ff8a1e38474e07f0174e06e2d
Johannes Förstner <johannes@famfoerstner.de>
2026-01-17 10:02:56 +0100

    Don't copy template folders named "original"

--- build/install_minibits.py
+++ build/install_minibits.py
@@ -18,7 +18,7 @@ def install_minibits_templates(bits: Bits, minibits_single_path: str):
     if os.path.exists(dst_templates_path):
         shutil.rmtree(dst_templates_path)
         time.sleep(0.1)  # shutil...
-    shutil.copytree(src_templates_path, dst_templates_path, ignore=ignore_patterns('demo'))
+    shutil.copytree(src_templates_path, dst_templates_path, ignore=ignore_patterns('demo', 'original'))
     time.sleep(0.1)  # shutil...
 
 
