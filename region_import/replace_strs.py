d5fb1bff1c57c42c54ce835bac7b8cc2f76b6915
Johannes Förstner <johannes@famfoerstner.de>
2026-01-31 17:18:38 +0100

    Proper arg-parsing; make more similar to replace_objs; add arg for --word

--- region_import/replace_strs.py
+++ region_import/replace_strs.py
@@ -1,20 +1,24 @@
+import argparse
 import os
 import sys
 import re
 
 from pathlib import Path
 
+from landscaping.replace_objs import parse_replacement_args
+
 
 # Primitive helper functions for gas files that treat them as mere text, not trying to parse anything.
 
 
-def replace_strs_in_file(file_path, strs: list[(str, str)], word=False) -> bool:
+def replace_strs_in_file(file_path, replacements: dict[str, str], word=False) -> bool:
     with open(file_path) as map_gas_file:
         text = map_gas_file.read()
     text_pre = text
-    for old_str, new_str in strs:
+    for old_str, new_str in replacements.items():
         if word:
-            text = re.sub(r"\b%s\b" % old_str, new_str, text)
+            if old_str in text:
+                text = re.sub(r"\b%s\b" % old_str, new_str, text)
         else:
             text = text.replace(old_str, new_str)
     if text != text_pre:
@@ -23,31 +27,43 @@ def replace_strs_in_file(file_path, strs: list[(str, str)], word=False) -> bool:
     return text != text_pre
 
 
-def replace_strs_in_dir(dir_path, strs: list[(str, str)], word=False, rglob_pattern='*.gas', print_path: bool = None):
+def replace_strs_in_dir(dir_path, replacements: dict[str, str], word=False, rglob_pattern='*.gas', print_path: bool = None):
     path_list = Path(dir_path).rglob(rglob_pattern)
     for path in path_list:
-        changed = replace_strs_in_file(path, strs, word)
+        changed = replace_strs_in_file(path, replacements, word)
         if print_path:
             print(path)
         elif print_path is None and changed:
             print(path)
 
 
-def replace_strs(path: str, strs: list[tuple[str, str]], word=False):
+def replace_strs(path: str, replacement_args: list[str], word=False):
+    replacements = parse_replacement_args(replacement_args)
     if os.path.isdir(path):
-        replace_strs_in_dir(path, strs, word)
+        replace_strs_in_dir(path, replacements, word)
     elif os.path.isfile(path):
-        replace_strs_in_file(path, strs, word)
+        replace_strs_in_file(path, replacements, word)
     else:
         assert False, f'{path} does not exist'
 
 
-def main(argv):
-    path = argv[0]
-    strs = [(str_str.split(',')[0], str_str.split(',')[1]) for str_str in argv[1].split(';')]
-    word = True
+def init_arg_parser():
+    parser = argparse.ArgumentParser(description='GasPy replace strs')
+    parser.add_argument('path')
+    parser.add_argument('replace', nargs='+')
+    parser.add_argument('--word', action='store_true')
+    return parser
 
-    replace_strs(path, strs, word)
+
+def parse_args(argv):
+    parser = init_arg_parser()
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
+
+    replace_strs(args.path, args.replace or list(), args.word)
 
     return 0
 
