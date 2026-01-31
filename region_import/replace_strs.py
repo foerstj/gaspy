9577a762c0b9fdc3b5a09436b8959fcfe9b39590
Johannes Förstner <johannes@famfoerstner.de>
2026-01-31 17:08:24 +0100

    Add option to replace whole words only

--- region_import/replace_strs.py
+++ region_import/replace_strs.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 from pathlib import Path
 
@@ -7,37 +8,47 @@ from pathlib import Path
 # Primitive helper functions for gas files that treat them as mere text, not trying to parse anything.
 
 
-def replace_strs_in_file(file_path, strs: list[(str, str)]) -> bool:
+def replace_strs_in_file(file_path, strs: list[(str, str)], word=False) -> bool:
     with open(file_path) as map_gas_file:
         text = map_gas_file.read()
     text_pre = text
     for old_str, new_str in strs:
-        text = text.replace(old_str, new_str)
+        if word:
+            text = re.sub(r"\b%s\b" % old_str, new_str, text)
+        else:
+            text = text.replace(old_str, new_str)
     if text != text_pre:
         with open(file_path, 'w') as map_gas_file:
             map_gas_file.write(text)
     return text != text_pre
 
 
-def replace_strs_in_dir(dir_path, strs: list[(str, str)], rglob_pattern='*.gas', print_path: bool = None):
+def replace_strs_in_dir(dir_path, strs: list[(str, str)], word=False, rglob_pattern='*.gas', print_path: bool = None):
     path_list = Path(dir_path).rglob(rglob_pattern)
     for path in path_list:
-        changed = replace_strs_in_file(path, strs)
+        changed = replace_strs_in_file(path, strs, word)
         if print_path:
             print(path)
         elif print_path is None and changed:
             print(path)
 
 
+def replace_strs(path: str, strs: list[tuple[str, str]], word=False):
+    if os.path.isdir(path):
+        replace_strs_in_dir(path, strs, word)
+    elif os.path.isfile(path):
+        replace_strs_in_file(path, strs, word)
+    else:
+        assert False, f'{path} does not exist'
+
+
 def main(argv):
     path = argv[0]
     strs = [(str_str.split(',')[0], str_str.split(',')[1]) for str_str in argv[1].split(';')]
-    if os.path.isdir(path):
-        replace_strs_in_dir(path, strs)
-    elif os.path.isfile(path):
-        replace_strs_in_file(path, strs)
-    else:
-        assert False, f'{path} does not exist'
+    word = True
+
+    replace_strs(path, strs, word)
+
     return 0
 
 
