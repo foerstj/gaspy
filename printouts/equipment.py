fd717d3af7fccae14c2727a8c6ed18d73474906d
Johannes Förstner <johannes@famfoerstner.de>
2026-02-17 16:08:26 +0100

    Distinguish vanilla vs. dsx

--- printouts/equipment.py
+++ printouts/equipment.py
@@ -9,8 +9,9 @@ from printouts.csv import write_csv_dict
 
 
 class Armor:
-    def __init__(self, template: Template):
+    def __init__(self, template: Template, is_dsx: bool):
         self._template = template
+        self.is_dsx = is_dsx
         self.template_name = template.name
         self.screen_name = template.compute_value('common', 'screen_name').strip('"')
         self.world_level, self.coverage, self.rarity = self.parse_template_name(self.template_name)
@@ -30,22 +31,36 @@ class Armor:
         return world_level, coverage, rarity
 
 
-def load_armors(bits: Bits) -> list[Template]:
-    return list(bits.templates.get_leaf_templates('armor').values())
+def load_dsx_armor_template_names(bits: Bits) -> list[str]:
+    templates = {}
+    template_base_dir = bits.templates.gas_dir
+    interactive_dir = template_base_dir.get_subdir(['regular', 'interactive'])
+    for gas_file_name, gas_file in interactive_dir.get_gas_files().items():
+        if gas_file_name.startswith('dsx_'):
+            bits.templates.load_templates_file(interactive_dir.get_gas_file(gas_file_name), templates)
+    # templates are unconnected but we only return the names anyway
+    return list(templates.keys())
 
 
-def process_armors(armor_templates: list[Template]):
-    return [Armor(armor_template) for armor_template in armor_templates if armor_template.name != 'gen_bd_un_bl_f_g_c_blood']
+def load_armor_templates(bits: Bits) -> tuple[list[str], list[Template]]:
+    dsx_armor_template_names = load_dsx_armor_template_names(bits)
+    armor_templates = list(bits.templates.get_leaf_templates('armor').values())
+    return dsx_armor_template_names, armor_templates
+
+
+def process_armors(armor_templates: list[Template], dsx_armor_template_names: list[str]):
+    return [Armor(armor_template, armor_template.name in dsx_armor_template_names) for armor_template in armor_templates if armor_template.name != 'gen_bd_un_bl_f_g_c_blood']
 
 
 def make_armors_csv(armors: list[Armor]):
-    keys = ['template', 'screen_name', 'world_level', 'coverage', 'rarity']
-    headers = {'template': 'Template', 'screen_name': 'Screen Name', 'world_level': 'World Level', 'coverage': 'Coverage', 'rarity': 'Rarity'}
+    keys = ['template', 'screen_name', 'is_dsx', 'world_level', 'coverage', 'rarity']
+    headers = {'template': 'Template', 'screen_name': 'Screen Name', 'is_dsx': 'LoA', 'world_level': 'World Level', 'coverage': 'Coverage', 'rarity': 'Rarity'}
     data = []
     for armor in armors:
         row = {
             'template': armor.template_name,
             'screen_name': armor.screen_name,
+            'is_dsx': 'LoA' if armor.is_dsx else None,
             'world_level': {'2w': 'Veteran', '3w': 'Elite'}.get(armor.world_level),
             'coverage': {'bd': 'Body', 'he': 'Helmet', 'bo': 'Boots', 'gl': 'Gloves', 'sh': 'Shield'}.get(armor.coverage),
             'rarity': {'ra': 'rare', 'un': 'unique'}.get(armor.rarity)
@@ -55,8 +70,8 @@ def make_armors_csv(armors: list[Armor]):
 
 
 def printout_equipment(bits: Bits):
-    armor_templates = load_armors(bits)
-    armors = process_armors(armor_templates)
+    dsx_armor_template_names, armor_templates = load_armor_templates(bits)
+    armors = process_armors(armor_templates, dsx_armor_template_names)
     armors_csv = make_armors_csv(armors)
     write_csv_dict('armors', *armors_csv)
 
