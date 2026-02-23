2dd54a3413094ec86ebdcac0e93589b9b50fb6ae
Johannes Förstner <johannes@famfoerstner.de>
2026-02-23 20:53:11 +0100

    Calculate the numbers

--- printouts/multiclass_amulets.py
+++ printouts/multiclass_amulets.py
@@ -17,6 +17,17 @@ SKILL_STAT = {
     'n': 'int',
     'c': 'int',
 }
+STAT_GAINS = {
+    'm': {'str': 0.64, 'dex': 0.27, 'int': 0.09},
+    'r': {'str': 0.25, 'dex': 0.62, 'int': 0.13},
+    'n': {'str': 0.09, 'dex': 0.18, 'int': 0.73},
+    'c': {'str': 0.13, 'dex': 0.17, 'int': 0.70},
+}
+
+
+def get_stats_at_skill_level(skill: str, level: int):
+    stat_gain = STAT_GAINS[skill]
+    return {s: g*level for s, g in stat_gain.items()}
 
 
 class MulticlassAmulet:
@@ -43,7 +54,19 @@ def calc_multiclass_amulets():
         for u in range(10, 70, 10):
             xp = level_xp[u]
             skill_levels = get_level(int(xp/2), level_xp)
+            stat_gains_a = get_stats_at_skill_level(a, u)
+            stat_gains_b = get_stats_at_skill_level(b, u)
+            stat_gains = {stat: (stat_gains_a[stat] + stat_gains_b[stat])/2 for stat in stat_gains_a}
+            stat_reqs = {stat: 10+gains for stat, gains in stat_gains.items()}
+            stat_gains_single_a = get_stats_at_skill_level(a, skill_levels)
+            stat_gains_single_b = get_stats_at_skill_level(b, skill_levels)
+            stat_gains_single = {stat: max(stat_gains_single_a[stat], stat_gains_single_b[stat]) for stat in stat_gains_a}
+            stat_gaps = {stat: stat_gains_single[stat] - stat_gains[stat] for stat in stat_gains}
+            stat_adds = {stat: stat_gaps[stat] if stat_gaps[stat] > 0 else None for stat in stat_gaps}
             print(f'{a}{b} u{u}: {xp} xp -> skill levels {skill_levels}')
+            print(f'  stat reqs {stat_reqs}')
+            print(f'  stat gaps {stat_gaps}')
+            print(f'  stat adds {stat_adds}')
 
     return [
         MulticlassAmulet('m', 'n', 20, 15, 15, 17, None, 18, 2.3, None, 2.75),
