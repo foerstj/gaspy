9a486976f0f957a30aba71607d465cf0d6e17290
Johannes Förstner <johannes@famfoerstner.de>
2026-05-30 19:57:00 +0200

    Printout the most obvious mismatches

--- printouts/frags.py
+++ printouts/frags.py
@@ -3,8 +3,12 @@ import argparse
 import sys
 
 from bits.bits import Bits
+from bits.moods import parse_float
 from gas.gas_parser import GasParser
-from printouts.common import parse_bool_value
+from printouts.common import parse_bool_value, parse_value
+
+
+GENERIC_TEXTURES = ['b_w_weapon']
 
 
 def frags(bits_path: str):
@@ -18,15 +22,24 @@ def frags(bits_path: str):
             continue
         gib_gore_good = parse_bool_value(enemy.compute_value('physics', 'gib_gore_good'))
         ewk_str = enemy.compute_value('physics', 'explode_when_killed')
-        explode_when_killed = False if ewk_str is None else parse_bool_value(ewk_str.split()[0])
+        explode_when_killed = False if ewk_str is None else parse_bool_value(ewk_str.split()[0])  # missing semicolon in krug_dog_skeleton
         if not gib_gore_good and not explode_when_killed:
             continue
         break_particulate = enemy.resolve_section('physics', 'break_particulate')
         if break_particulate is None:
             continue
         frag_names = [a.name for a in break_particulate.get_attrs()]
-        frag_names_str = ', '.join(frag_names)
-        print(f'{enemy.name}: {gib_gore_good} {explode_when_killed}; {frag_names_str}')
+        actor_texture = enemy.compute_value('aspect', 'textures', '0')
+        actor_scale = parse_value(enemy.compute_value('aspect', 'scale_base'))
+        frag_templates = [bits.templates.templates[f.lower()] for f in frag_names]
+        frag_textures = [f.compute_value('aspect', 'textures', '0') for f in frag_templates]
+        frag_scales = [parse_value(f.compute_value('aspect', 'scale_base')) for f in frag_templates]
+        texture_mismatch = None if actor_texture is None else any([t is not None and t not in GENERIC_TEXTURES and t != actor_texture for t in frag_textures])
+        scale_mismatch = None if actor_scale is None else any([s is not None and s != actor_scale for s in frag_scales])
+        if texture_mismatch:
+            print(f'{enemy.name}: texture mismatch: {actor_texture} != {set(frag_textures)}')
+        if scale_mismatch:
+            print(f'{enemy.name}: scale mismatch: {actor_scale} != {set(frag_scales)}')
 
 
 def init_arg_parser():
