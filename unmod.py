de6e8b8dccd3ab290d4e62f67fec9b709d866282
Johannes Förstner <johannes@famfoerstner.de>
2026-06-20 22:32:47 +0200

    Make it work somewhat

--- unmod.py
+++ unmod.py
@@ -2,11 +2,12 @@ import argparse
 import sys
 
 from bits.bits import Bits
+from bits.templates import Template
 
 
 def unmod(template_path: str, bits_path: str):
     bits = Bits(bits_path)
-    bits.templates.get_templates()
+    # bits.templates.get_templates()
 
     path_segs = template_path.split('/')
     gas_dir = bits.templates.gas_dir
@@ -19,10 +20,33 @@ def unmod(template_path: str, bits_path: str):
             bits.templates.load_templates_rec_files(gas_dir, templates)
         elif gas_dir.has_gas_file(last_seg):
             bits.templates.load_templates_file(gas_dir.get_gas_file(last_seg), templates)
-    templates = [bits.templates.templates[template_name] for template_name in templates]
-    templates = [t for t in templates if t.is_leaf() and t.is_equipment()]
 
-    print([t.name for t in templates])
+    # templates = [bits.templates.templates[template_name] for template_name in templates]
+    for name, template in templates.items():
+        if template.specializes is not None:
+            parent_template = templates.get(template.specializes.lower())
+            if parent_template is None:
+                continue
+            template.parent_template = parent_template
+            parent_template.child_templates.append(template)
+    templates = templates.values()
+    templates = [t for t in templates if t.is_leaf()]
+    # templates = [t for t in templates if t.is_equipment()]
+
+    for template in templates:
+        assert isinstance(template, Template)
+        existing = template.section.resolve_attr('common', 'is_pcontent_allowed')
+        if existing is not None:
+            existing.value = False
+            continue
+
+        commons = template.section.get_sections('common')
+        if len(commons) > 0:
+            common = commons[-1]
+        else:
+            common = template.section.get_or_create_section('common')
+        common.set_attr_value('is_pcontent_allowed', False)
+    gas_dir.save()
 
 
 def parse_args(argv: list[str]):
